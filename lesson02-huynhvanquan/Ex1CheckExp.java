e1a7dbdf8a136525b652c604c5dd8a83b00fa31e
huynhquanhht <huynhquanhht@gmail.com>
2021-05-03 14:16:43 +0700

    lesson02-huynhvanquan 02.05.2021

---
+++ lesson02-huynhvanquan/Ex1CheckExp.java
@@ -0,0 +1,43 @@
+package homework;
+
+import java.util.Scanner;
+import data.Digit;
+
+public class Ex1CheckExp {
+	public static void main(String[] args) {
+		int n;
+		String str;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter n: ");
+		str = sc.nextLine();
+		
+		while (!isDigit(str)) {
+			System.out.println("Please enter integer! \nEnter n: ");
+			str = sc.nextLine();
+		}
+		
+		if (checkExp(Integer.parseInt(str))) {
+			System.out.println(str + " là lũy thừa của 2.");
+		} else {
+			System.out.println(str + " không phải lũy thừa của 2.");
+		}
+	}
+	
+	static Boolean isDigit(String str) {
+		if (str.length() == 0) {
+			return false;
+		}
+		for (char i : str.toCharArray()) {
+			if( i < '0' || i > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	static Boolean checkExp(int n) {
+		return (int)(Math.ceil((Math.log(n) / Math.log(2)))) 
+	            == (int)(Math.floor(((Math.log(n) / Math.log(2)))));
+	}
+}
