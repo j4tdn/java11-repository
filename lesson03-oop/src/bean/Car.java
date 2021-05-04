378d5228db2f0802c56ad90edb0da2eac2022e0f
phu <levanphu0801@gmail.com>
2021-05-04 22:55:23 +0700

    lesson03-oop 04.05.2021

---
+++ lesson03-oop/src/bean/Car.java
@@ -0,0 +1,59 @@
+package bean;
+
+public class Car {
+	private String serial;
+	private String model;
+	private String color;
+
+	public Car() {
+
+	}
+
+	public Car(String serial, String model, String color) {
+		this.serial = serial;
+		this.model = model;
+		this.color = color;
+	}
+
+	public String getSerial() {
+		return serial;
+	}
+
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Car)) {
+			return false;
+		}
+		Car that = (Car) obj;
+		return this.getSerial().equals(that.getSerial());
+	}
+
+	@Override
+	public String toString() {
+		return "Car [serial=" + serial + ", model=" + model + ", color=" + color + "]";
+	}
+
+}
