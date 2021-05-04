f81dc927e8a7d902fc7f59f721ca4290fe8c515c
HoangSon <hthaison17@gmail.com>
2021-05-04 23:58:34 +0700

    lesson03-oop 04.05.2021

---
+++ lesson03-oop/src/demo/Ex01.java
@@ -0,0 +1,17 @@
+package demo;
+
+import bean.Rectangle;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		Rectangle r1 = new Rectangle();
+		r1.setwidth(12);
+		r1.setHeight(6);
+		System.out.println("r1: " + r1);
+
+		Rectangle r2 = new Rectangle(9, 7);
+		System.out.println("r2: " + r2);
+
+	}
+
+}
