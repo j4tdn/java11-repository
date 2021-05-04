655a637c043c998ef0bdf809dcd778e97f8eb39f
ncchuong <ncchuong99@gmail.com>
2021-05-04 23:00:37 +0700

    lesson03-oop 04.05.2021

---
+++ lesson03-oop/src/demo/Ex02.java
@@ -0,0 +1,16 @@
+package demo;
+
+import bean.Rectangle;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Integer a = 10; // +
+		String b = new String("hello"); // +
+		float c = 10.2f;
+		Double d = 7d; // +
+		Rectangle r1 = null; 
+		Rectangle r2 = new Rectangle(); // +
+		Rectangle r3 = r2;
+		String f = "welcome"; // +
+	}
+}
---
+++ lesson03-oop/src/demo/Ex04Static.java
@@ -0,0 +1,18 @@
+package demo;
+
+import bean.CarAd;
+
+public class Ex04Static {
+	public static void main(String[] args) {
+		CarAd.model = "Audi";
+		
+		CarAd ad1 = new CarAd("green");
+		CarAd ad2 = new CarAd("blue");
+
+		ad1.setColor("yellow");
+		CarAd.model = "Mazda";
+
+		System.out.println("ad1: " + ad1);
+		System.out.println("ad2: " + ad2);
+	}
+}
