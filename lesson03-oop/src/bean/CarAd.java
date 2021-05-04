16ba08a0dd2b61dc0e2daa42fd34a2a651d9d84a
hieunguyen <hieun614@gmail.com>
2021-05-05 00:32:10 +0700

    lesson03-oop 05.05.2021

---
+++ lesson03-oop/src/bean/CarAd.java
@@ -0,0 +1,25 @@
+package bean;
+
+public class CarAd {
+	public static String model;
+
+	private String color;
+	
+	
+
+	public CarAd(String color) {
+		this.color = color;
+	} 
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+	@Override
+	public String toString() {
+		return model + " , " + color;
+	}
+}
