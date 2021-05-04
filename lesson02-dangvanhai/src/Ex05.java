e489d9a6dd3e4a16a4d5755eecaf6c5a934e6918
vhuy <vanhuy0001dtu@gmail.com>
2021-05-04 23:10:38 +0700

    delete wrong projects

--- lesson02-dangvanhai/src/Ex05.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Ex05 {
-	public static void main(String[] args) {
-		Scanner ip = new Scanner(System.in);
-		System.out.println(Integer.toBinaryString(ip.nextInt()));
-	}
-}
