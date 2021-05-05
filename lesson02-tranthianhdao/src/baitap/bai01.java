15be3dcd4db23e19169d16698d83c3de046a9a2b
dtran <trananhdao276@gmail.com>
2021-05-06 00:09:56 +0700

    lesson02-trananhdao 06.05.2021

---
+++ lesson02-tranthianhdao/src/baitap/bai01.java
@@ -0,0 +1,35 @@
+package baitap;
+
+import java.util.Scanner;
+
+public class bai01 {
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		int n;
+		do {
+			System.out.print("Nhap n: ");
+			n = ip.nextInt();
+			if (n <= 0) {
+				System.out.println("Nhap sai. Moi nhap lai!\n");
+				continue;
+			}
+			if (LaLuyThua2(n)) {
+				System.out.println(n + " la luy thua cua 2");
+			} else {
+				System.out.println(n + " khong la luy thua cua 2");
+			}
+		} while (n <= 0);
+	}
+
+	private static boolean LaLuyThua2(int n) {
+		while (n > 0 && n % 2 == 0) {
+			n /= 2;
+		}
+		if (n == 1) {
+			return true;
+		}
+		return false;
+
+	}
+
+}
---
+++ lesson02-tranthianhdao/src/baitap/bai07_c2.java
@@ -0,0 +1,30 @@
+package baitap;
+
+import java.util.Random;
+
+public class bai07_c2 {
+	public static void main(String[] args) {
+		int[] data = new int[5];
+		int a, i = 0;
+		Random rd = new Random();
+
+		do {
+			a = 20 + rd.nextInt(30 - 20) + 1;
+			if (contains(data, a)) {
+				continue;
+			}
+			System.out.print(a+" ");
+			data[i++] = a;
+		} while (i < 5);
+	}
+
+	private static boolean contains(int[] data, int a) {
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] == a) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
