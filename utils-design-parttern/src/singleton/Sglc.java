0e4a528b3975caffd4b052842c0934a6ca75df13
huongtruong <truongmyhuong99@gmail.com>
2021-05-11 23:18:06 +0700

    lesson04-oop-11.05.2021

---
+++ utils-design-parttern/src/singleton/Sglc.java
@@ -0,0 +1,24 @@
+package singleton;
+
+public class Sglc {
+	private static Sglc instance = null;
+	private String value;
+	
+	public static Sglc getInstance() {
+		if ( instance ==null) {
+			instance = new Sglc();
+		}
+		return instance;
+	}
+	private Sglc() {
+		
+	}
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
