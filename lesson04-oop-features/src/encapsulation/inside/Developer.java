b4a46e028bc85f36becacb3ea69d384c3ad637fd
hpham <binhmt2008@gmail.com>
2021-05-11 22:47:18 +0700

    lesson04-oop-features 11.05.2021

---
+++ lesson04-oop-features/src/encapsulation/inside/Developer.java
@@ -0,0 +1,8 @@
+package encapsulation.inside;
+
+//parent class
+public class Developer {
+	protected String getSkills() {
+		return "JAVA";
+	}
+}
---
+++ utils-design-pattern/src/singleton/SglcApp.java
@@ -0,0 +1,22 @@
+package singleton;
+
+public class SglcApp {
+	public static void main(String[] args) {
+		Sglc s1 = Sglc.getInstance();
+		Sglc s2 = Sglc.getInstance();
+
+		s1.setValue("HELLO  - 2021");
+		
+		System.out.println(s1.getValue());
+		System.out.println(s2.getValue());
+		
+		System.out.println("s1: " + System.identityHashCode(s1));
+		System.out.println("s2: " + System.identityHashCode(s2));
+		
+		//Can we create a type with private constructor?
+		//1. Enumeration
+		//2. Utility class
+		//3. Singleton pattern
+		
+	}
+}
