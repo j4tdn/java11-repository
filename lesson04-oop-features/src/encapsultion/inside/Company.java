b0fe1bafee7f51be0800918c43c2c693a32ee53e
HoangSon <hthaison17@gmail.com>
2021-05-11 22:42:28 +0700

    lesson04-oop-features 11.05.2021

---
+++ lesson04-oop-features/src/encapsultion/inside/Company.java
@@ -0,0 +1,9 @@
+package encapsultion.inside;
+
+public class Company {
+	private String id;
+	public String name;
+	protected double age;
+	String address;
+
+}
---
+++ lesson04-oop-features/src/encapsultion/inside/Developer.java
@@ -0,0 +1,8 @@
+package encapsultion.inside;
+
+public class Developer {
+	protected String getSkill() {
+		return "Java";
+	}
+	
+}
