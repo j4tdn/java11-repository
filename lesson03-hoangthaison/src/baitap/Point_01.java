27ec47cddd1eec3e3ff7e5aa8f0d13372789abd7
HoangSon <hthaison17@gmail.com>
2021-05-11 16:31:51 +0700

    lesson03-hoangthaison 11-5-2021

---
+++ lesson03-hoangthaison/src/baitap/Point_01.java
@@ -0,0 +1,32 @@
+package baitap;
+
+public class Point_01 {
+	public double x;
+	public double y;
+	public Point_01() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Point_01 [x=" + x + ", y=" + y + "]";
+	}
+	public Point_01(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}	
+	
+
+}
