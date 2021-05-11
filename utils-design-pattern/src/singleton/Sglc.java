e136c666e36ffaf7544111546d07be7cf29e0055
nhanndp <phuocnhan1994@gmail.com>
2021-05-11 22:38:53 +0700

    utils-design-pattern 11.05.2021

---
+++ utils-design-pattern/src/singleton/Sglc.java
@@ -0,0 +1,32 @@
+package singleton;
+
+/**
+ * Singleton is a creational design pattern that lets you ensure that a class has only one instance, 
+ * while providing a global access point to this instance
+ */
+
+public class Sglc {
+	
+	private static Sglc instance =null;
+	
+	private String value;
+
+	public static Sglc getInstance() {
+		if(instance ==null) {
+			instance = new Sglc();
+		}
+		return instance;
+	}
+	
+	private Sglc() {
+		
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}	
+}
