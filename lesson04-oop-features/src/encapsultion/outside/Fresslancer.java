e092928e88abb9d85fd4769af023ed4b7f9a7e15
HoangSon <hthaison17@gmail.com>
2021-05-13 21:54:48 +0700

    savechange 13-5-2021

--- lesson04-oop-features/src/encapsultion/outside/Fresslancer.java
+++ lesson04-oop-features/src/encapsultion/outside/Fresslancer.java
@@ -1,5 +1,15 @@
 package encapsultion.outside;
 
-public class Fresslancer {
+import encapsultion.inside.Company;
+import encapsultion.inside.Developer;
+
+public class Fresslancer extends Developer {
+	public static void main(String[] args) {
+		Company comp = new Company();
+		
+		comp.name = "mgm";
+		Fresslancer f1 = new Fresslancer();
+		f1.getSkill();
+	}
 
 }
--- utils-design-pattern/src/singleton/SglcApp.java
+++ utils-design-pattern/src/singleton/SglcApp.java
@@ -4,7 +4,7 @@ public class SglcApp {
 	public static void main(String[] args) {
 		Sglc s1  = Sglc.getInstance();
 		Sglc s2 = Sglc.getInstance();
-		
+		s1.setValue("Hello-2021");
 		System.out.println("s1: "+ System.identityHashCode(s1));
 		System.out.println("s2: "+ System.identityHashCode(s2));
 	}
