f5b33a61a66dd800db191c54315aac79cfb3287f
nuhigin <nuhigin@gmail.com>
2021-05-16 00:51:32 +0700

    lesson02-nhuhieu 16.05.2021(add Exercises 04)

--- lesson02-thannhuhieu/src/homeworks/Ex04.java
+++ lesson02-thannhuhieu/src/homeworks/Ex04.java
@@ -1,5 +1,23 @@
 package homeworks;
 
 public class Ex04 {
+	public static void main(String[] args) {
+		int[] a = { 5, 1, 9, 11, 20, 19, 17, 21, 30 };
+		int max = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 2 != 0 && max < a[i])
+				;
+			max = a[i];
+		}
+		System.out.println("max: " + max);
+
+		int secondmax = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 2 != 0 && secondmax < a[i] && a[i] < max)
+				secondmax = a[i];
+		}
+		System.out.println("secondmax: " + secondmax);
+
+	}
 
 }
