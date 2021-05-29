de9bfab9ed03f4c3c4c4972274334250806e3618
nhanndp <phuocnhan1994@gmail.com>
2021-05-29 22:58:30 +0700

    lesson06 29.05.2021

---
+++ lesson04-oop-features/src/functional/EvenCondition.java
@@ -0,0 +1,10 @@
+package functional;
+
+public class EvenCondition implements Condition {
+	@Override
+	public boolean test(int a) {
+		System.out.println("EvenCondition: " + a);
+		return a % 2 == 0;
+	}
+
+}
