49d9a4f37ede09c3457b35f81c5414aa0537a6a7
HoangSon <hthaison17@gmail.com>
2021-06-01 22:41:36 +0700

    lesson06-exception hthaison 1-6-2021

--- lesson06-exception/src/demo/Ex02.java
+++ lesson06-exception/src/demo/Ex02.java
@@ -1,16 +1,28 @@
 package demo;
 
+import exception.ZeroException;
+
 public class Ex02 {
 	public static void main(String[] args) {
-		int a = 10; 
-		int b = 5;
-		
-		System.out.println("result: "+(devide(a,b)));
-		
-	}
-	private static int devide(int a, int b) {
-		return a/b;
-		
+		System.out.println("Main start");
+		int a = 10;
+		int b = 0;
+
+		try {
+			System.out.println("result:  " + divide(a, b));
+		} catch (ZeroException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Main end");
 	}
 
+	private static int divide(int a, int b) throws ZeroException {
+		// // throws RuntimeException >> no mandatory to handle exception
+		if (b == 0) {
+			// throw new ArithmeticException();
+			throw new ZeroException("Please enter a non-zero value");
+		}
+		System.out.println("return a / b");
+		return a / b;
+	}
 }
