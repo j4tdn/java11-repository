a9db75c9b0f6666ea5b5b8c0db56edd412b0fbd4
vhuy <vanhuy0001dtu@gmail.com>
2021-06-01 22:40:16 +0700

    lesson06-exception 01.06.2021

--- lesson06-exception/src/demo/Ex01.java
+++ lesson06-exception/src/demo/Ex01.java
@@ -8,10 +8,27 @@ public class Ex01 {
 		System.out.println("Start");
 		Scanner ip = new Scanner(System.in);
 		System.out.println("Enter yob: ");
-		//int yob = ip.nextInt(); //InputMismatchException
-		int yob = Integer.parseInt(ip.nextLine()); //NumberFormatException
+		int yob = 0;
+		int count = 0;
+		while (true) {
+			try {
+				// int yob = ip.nextInt(); //InputMismatchException
+				yob = Integer.parseInt(ip.nextLine()); // NumberFormatException
+				break;
+			} catch (NumberFormatException e) {
+				// contains exception info & root cause
+				// System.out.println(e.getMessage());
+				// e.printStackTrace();
+				System.out.println("Invalid number " + ++count + "times");
+				if(count == 3) {
+					System.out.println("Wrong entering times exceed 3 times");
+				}
+			}
+		}
 		
-		System.out.println("Age: " + (LocalDate.now().getYear() - yob + 1));
+		if(yob != 0) {
+			System.out.println("Age: " + (LocalDate.now().getYear() - yob + 1));
+		}
 		System.out.println("End");
 	}
 }
