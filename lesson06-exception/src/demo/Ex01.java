4bff3ca6e2e610abb757da66400a8324ea202b29
vhoang <leviethoang1082000@gmail.com>
2021-06-01 22:42:22 +0700

    lesson06-exception 01.06.2021

--- lesson06-exception/src/demo/Ex01.java
+++ lesson06-exception/src/demo/Ex01.java
@@ -7,10 +7,29 @@ public class Ex01 {
 	public static void main(String[] args) {
 		System.out.println("Main Start");
 		Scanner ip = new Scanner(System.in);
-		System.out.print("Enter year of birth : ");
-		int yob = ip.nextInt();
-		
-		System.out.println("Age :" + (LocalDate.now().getYear() - yob + 1));
+		int yob = 0, count = 0;
+		while (true) {
+			try {
+				System.out.print("Enter year of birth : ");
+				yob = Integer.parseInt(ip.nextLine());
+				System.out.println("==========Ok============");
+				break;
+			} catch (NumberFormatException e) {
+				// contains exception information & root cause
+				// System.out.println(e.getMessage());
+				
+				System.out.println("Invalid number " + ++count  + " times");
+				if( count == 3) {
+					System.out.println("==========================================");
+					System.out.println("Wrong entering times exceed 3 times >> OUT");
+					break;
+				}
+			}
+		}
+
+		if (yob != 0) {
+			System.out.println("Age :" + (LocalDate.now().getYear() - yob + 1));
+		}
 		System.out.println("Main End");
 	}
 }
--- lesson06-exception/src/demo/Ex02.java
+++ lesson06-exception/src/demo/Ex02.java
@@ -1,15 +1,30 @@
 package demo;
 
+import exception.ZeroException;
+
 public class Ex02 {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 0;
+		try {
+			System.out.println("Result : " + divide(a, b));
+		} catch (ZeroException e) {
+			// System.out.println("Message" + e.getMessage();
+		e.printStackTrace();
+		}
 		
-		System.out.println("Result : " + divide(a, b));
 	}
 	
-	private static int divide(int a, int b) {
-		// ArithmeticException
+	private static int divide(int a, int b)  throws ZeroException{
+		// ArithmeticException 
+		// throws RuntimeException >> no manition  
+		if(b == 0) {
+			// ZeroException
+			System.out.println("Throw new");
+			//throw new ArithmeticException();
+			throw new ZeroException("Please enter a non-zero value");
+		}
+		System.out.println("a / b");
 		return a / b;
 	}
 }
