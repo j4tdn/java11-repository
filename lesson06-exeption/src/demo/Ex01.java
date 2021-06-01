82a3f55ab24ed6c1e4322322e7cdc9e063fa106f
dtran <trananhdao276@gmail.com>
2021-06-01 22:49:26 +0700

    lesson06-exeption 01.06.2021

--- lesson06-exeption/src/demo/Ex01.java
+++ lesson06-exeption/src/demo/Ex01.java
@@ -7,15 +7,35 @@ public class Ex01 {
 	public static void main(String[] args) {
 		System.out.println("Main start");
 		Scanner ip = new Scanner(System.in);
-		
-		System.out.println("Enter year of birth: ");
-	//	int yob = ip.nextInt();
-		int yob = Integer.parseInt(ip.nextLine());
-		
-		
-		System.out.println("Age: " + (LocalDate.now().getYear() - yob +1));
-		System.out.println("Main end " );
-		
+
+		// int yob = ip.nextInt();
+		int yob = 0;
+		int count = 0;
+		while (true) {
+			
+			try {
+				System.out.println("Enter year of birth: ");
+				yob = Integer.parseInt(ip.nextLine());
+				break;
+
+			} catch (NumberFormatException e) {
+
+				// System.out.println("Hello Exception - I'm Block Catcher");
+				// e.printStackTrace();
+				// System.out.println(e.getMessage());
+				System.out.println("Invalid number " + ++count + " times");
+				if (count == 3) {
+					System.out.println("===================================== ");
+					System.out.println("Wrong entering times exeed 3 times>>OUT");
+				}
+			}
+
+		}
+
+		System.out.println("Age: " + (LocalDate.now().getYear() - yob + 1));
+
+		System.out.println("Main end ");
+
 	}
 
 }
--- lesson06-exeption/src/demo/Ex02.java
+++ lesson06-exeption/src/demo/Ex02.java
@@ -1,16 +1,29 @@
 package demo;
 
+import exception.ZeroException;
+
 public class Ex02 {
 	public static void main(String[] args) {
 		int a = 5;
-		int b = 10;
-		
-		System.out.println("result: " + divide(a,b));
+		int b = 0;
+
+		try {
+			System.out.println("result: " + divide(a, b));
+		} catch (ZeroException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Main End");
 
 	}
 
 	private static int divide(int a, int b) {
-		return a/b;
 		
+		if(b==0) {
+			throw new ZeroException("Please enter a non-zero value");
+			
+		}
+		System.out.println("return a/b");
+		return a / b;
+
 	}
 }
