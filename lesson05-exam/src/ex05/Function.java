77a52cdeaf3f03e8c6810d4d4134792ef0625315
Quyen Phan <Quyen.Phan@mgm-tp.com>
2021-06-05 22:27:56 +0700

    lesson05-exam 05.06.2021

---
+++ lesson05-exam/src/ex05/Function.java
@@ -0,0 +1,8 @@
+package ex05;
+
+@FunctionalInterface
+public interface Function {
+	
+	// strategy
+	boolean test(Book book);
+}
---
+++ lesson05-exam/src/ex05/ReferenceBook.java
@@ -0,0 +1,26 @@
+package ex05;
+
+public class ReferenceBook extends Book{
+	private double tax;
+	
+	public ReferenceBook() {
+	}
+
+	public ReferenceBook(String id, double cost, String publisher, double tax) {
+		super(id, cost, publisher);
+		this.tax = tax;
+	}
+
+	public double getTax() {
+		return tax;
+	}
+	
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " >> " + tax;
+	}
+}
