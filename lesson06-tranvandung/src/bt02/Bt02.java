dc60ea968d6fa67fc500ea4674f97a56e9ef2c3e
teoven <68175492+teoven@users.noreply.github.com>
2021-06-08 20:03:49 +0700

    lesson06

---
+++ lesson06-tranvandung/src/bt02/Bt02.java
@@ -0,0 +1,7 @@
+package bt02;
+
+public class Bt02 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ lesson06-tranvandung/src/bt03/Bt03.java
@@ -0,0 +1,31 @@
+package bt03;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Bt03 {
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		String input;
+
+		String regex = "^\\w*+@{1}+\\w*+.com$";
+
+		while (true) {
+			try {
+				System.out.print("Enter mail: ");
+				input = ip.nextLine();
+				Pattern pattern = Pattern.compile(regex);
+				Matcher matcher = pattern.matcher(input);
+				if (matcher.find()) {
+					System.out.println("Valid email");
+					break;
+				}else {
+					System.out.println("Invalid email, try again");
+				}
+			} catch (Exception e) {
+				input = ip.nextLine();
+			}
+		}
+	}
+}
