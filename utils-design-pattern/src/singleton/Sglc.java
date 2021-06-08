ade19e6c0e62bc2c82ee4909dbdd6617f62fa830
Quyen Phan <Quyen.Phan@mgm-tp.com>
2021-06-08 22:37:21 +0700

    lesson07-libraries 08.06.2021

--- utils-design-pattern/src/singleton/Sglc.java
+++ utils-design-pattern/src/singleton/Sglc.java
@@ -3,7 +3,7 @@ package singleton;
 public class Sglc {
 
 	/**
-	 * Singleton is a creational design pattern that lets you ensure that a class has only one instance, 
+	 * Singleton is a creation design pattern that lets you ensure that a class has only one instance, 
 	 * while providing a global access point to this instance
 	 */
 	private static Sglc instance = null;
