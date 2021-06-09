9791aae10a8f7e1e61087d71d58a51ea6812ffbd
HoangSon <hthaison17@gmail.com>
2021-06-09 22:40:12 +0700

    lesson06-hoangthaison 9-6-2021

---
+++ lesson06-hoangthaison/src/exception/LengthOfPassException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class LengthOfPassException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6410166370867965144L;
+
+	public LengthOfPassException(String message) {
+		super(message);
+	}
+	
+}
---
+++ lesson06-hoangthaison/src/exception/SpecialCharacterException.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class SpecialCharacterException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5570638137962309543L;
+	
+	public SpecialCharacterException(String Message) {
+		super(Message);
+	}
+	
+	
+
+}
