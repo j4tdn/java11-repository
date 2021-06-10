92e0f25557efeb8e2cba27c3d54df8e34181b7dd
trungvo <trung16vlxd@gmail.com>
2021-06-10 23:34:54 +0700

    Edit code lesson03 10.06.2020

--- lesson03-opp/src/demo/Ex01.java
+++ lesson03-opp/src/demo/Ex01.java
@@ -1,5 +1,18 @@
 package demo;
 
-public class Ex01 {
+import bean.Rectangle;
 
+public class Ex01 {
+	public static void main(String[] args) {
+		//h8, w4
+		Rectangle r1 = new Rectangle();
+		r1.setHeight(8);
+		r1.setWidth(4);
+		System.out.println("r1: " + r1);
+		
+		//h10,w12
+		Rectangle r2 = new Rectangle(10,12);
+		System.out.println("r2: " + r2);
+		System.out.println(r1 instanceof Rectangle);
+	}
 }
