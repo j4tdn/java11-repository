0d994e53d8ecfeb7cb96928883b5c843d6d1164e
HoangSon <hthaison17@gmail.com>
2021-06-10 20:05:36 +0700

    lesson06-hthaison 10-6-2021

--- lesson06-hoangthaison/src/ex03/App.java
+++ lesson06-hoangthaison/src/ex03/App.java
@@ -1,5 +1,24 @@
 package ex03;
 
-public class App {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class App {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean check;
+		System.out.println("Email: ");
+		try {
+		String mail = sc.nextLine();
+		check = Pattern.matches("[^@][0-9a-zA-Z]*@([a-z]+\\\\.[a-z]+|[a-z]+\\\\.[a-z]+\\\\.[a-z]+)", mail);
+		if(check) {
+			System.out.println("OK");
+		}
+		else System.out.println("KO");
+		}
+		catch(RuntimeException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
