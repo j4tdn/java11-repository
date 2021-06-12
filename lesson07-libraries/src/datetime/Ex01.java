da81ab379839ef29ef5f70b1502014eddf4caf04
HoangSon <hthaison17@gmail.com>
2021-06-12 22:30:33 +0700

    lesson07-libraries hoangthaison 12-06-2021

--- lesson07-libraries/src/datetime/Ex01.java
+++ lesson07-libraries/src/datetime/Ex01.java
@@ -1,74 +1,43 @@
 package datetime;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.time.Month;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
-
-import utils.DateUtils;
 
 public class Ex01 {
+	private static String[] months = { "JAN", "FEB", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEP", "OCT",
+			"NOV", "DEC" };
 
-	private static String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEP", "OCT", "NOV", "DEC"};
-	private static DateFormat df = new SimpleDateFormat("dd/MM/yy EEEE");
-	
-	
 	public static void main(String[] args) {
-		// JAVA07: Month 0 - 11 <<=>> JAN - DEC
 		Date date = new Date();
 		System.out.println("date: " + date);
-		
-		Locale.setDefault(Locale.FRANCE);
+
 		Calendar c = Calendar.getInstance();
-		
-		System.out.println("calendar: " + c);
-		System.out.println("==================");
-		int day   = c.get(Calendar.DAY_OF_MONTH);
+		System.out.println("calender: " + c);
+		int day = c.get(Calendar.DAY_OF_MONTH);
 		int month = c.get(Calendar.MONTH);
-		int year  = c.get(Calendar.YEAR);
-		
-		// 8-June-2021
-		System.out.println("d-m-y: " + day + "-" + months[month] + "-" + year);
-		
+		int year = c.get(Calendar.YEAR);
+
+		Month month1 = Month.of(5);
+		System.out.println(month1);
+
+		System.out.println("d/m/y: " + day + "/" + month + "/" + year);
+		System.out.println("d/m/y: " + day + "/" + month + "/" + year);
+
 		int hour12 = c.get(Calendar.HOUR);
 		int hour24 = c.get(Calendar.HOUR_OF_DAY);
 		int minute = c.get(Calendar.MINUTE);
 		int second = c.get(Calendar.SECOND);
-		System.out.println("h:m:s >> " + hour12 + ":" + minute + ":" + second );
-	
-		// number of days in month, year
+		System.out.println("h:m:s  " + hour12 + ":" + minute + ":" + second);
+
 		int daysInMonth = c.getActualMaximum(Calendar.DAY_OF_MONTH);
-		System.out.println("days in month: " + daysInMonth);
-		
-		int dow = c.get(Calendar.DAY_OF_WEEK);
-		System.out.println("dow: " + dow);
-		
-		// first day of week: default: SUNDAY
-		System.out.println("Locale: " + Locale.getDefault());
-		
-		int fdow = c.getFirstDayOfWeek();
-		System.out.println("fdow: " + fdow);
-		
-		// fdow_num = current_date + fdow - current_dow
-		
-		c.add(Calendar.DAY_OF_MONTH, fdow - c.get(Calendar.DAY_OF_WEEK));
-		System.out.println("c: " + c);
-		// fdow_num = 6.6.2021
-		
-		System.out.println("==========================");
-		
-		int dayIndex = 1;
-		while(dayIndex <= 7) {
-			System.out.println(df.format(c.getTime()));
-			c.add(Calendar.DAY_OF_MONTH, 1);
-			dayIndex++;
-		}
-		
-		c.set(Calendar.YEAR, 2020);
-		System.out.println("isLeapYear: " + DateUtils.isLeapYear(c.get(Calendar.YEAR)));
-	
-		System.out.println("c: " + c);
-		System.out.println("FROM EPOCH: " + c.getTimeInMillis());
-		}
+		System.out.println(daysInMonth);
+
+		// First day of week
+		System.out.println(c.getFirstDayOfWeek());
+
+		//
+		int dow = c.get(Calendar.FRIDAY);
+
 	}
+}
--- lesson07-libraries/src/utils/DateUtils.java
+++ lesson07-libraries/src/utils/DateUtils.java
@@ -1,16 +1,100 @@
 package utils;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class DateUtils {
-	private int a;
-	private int b;
+	private static final String DATE_PATTERN = "dd/MM/yyyy";
+	private static final String DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm:ss EEEE";
+	private static String[] months = { "JAN", "FEB", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEP", "OCT",
+			"NOV", "DEC" };
+	private static String[] weekdays = { "SUN", "MON", "TUE", "WEB", "THUR", "FRI", "SAT" };
+	private static DateFormat df = new SimpleDateFormat(DATE_PATTERN);
+	private static DateFormat dtf = new SimpleDateFormat(DATE_TIME_PATTERN);
 	private static GregorianCalendar gc = new GregorianCalendar();
-	
+
 	private DateUtils() {
-		
 	}
+
 	public static boolean isLeapYear(int year) {
 		return gc.isLeapYear(year);
+	}
+
+	public static String getWeekDayAsString(int weekDayAsNumber) {
+		// 1 - 7
+		//
+		//
+		return weekdays[weekDayAsNumber - 1];
+	}
+
+	public static Date toDate(Calendar c) {
+		Objects.requireNonNull(c);
+		return c.getTime();
+	}
+
+	/**
+	 * Convert to Date from given text with format dd/MM/yyyy
+	 * 
+	 * @param text input
+	 * @return converted Date
+	 */
+	public static Date toDate(String text) {
+		if (text == null || text.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		Date date = null;
+		try {
+			date = df.parse(text);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date;
+	}
+
+	/**
+	 * Convert to Calendar from given date E.g: 10.10.1990 >> Epoch time. 01.01.1970
+	 * >> Date Calendar
+	 * 
+	 * @param date
+	 * @return converted Calendar
+	 */
+	public static Calendar toCalendar(Date date) {
+		Objects.requireNonNull(date);
+		Calendar c = Calendar.getInstance();
+		// long dateInMillis = date.getTime();
+		// c.setTimeInMillis(dateInMillis);
+		c.setTime(date);
+		return c;
+	}
+
+	/**
+	 * Get calendar from given year, month, day
+	 * 
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @return calendar
+	 */
+	public static Calendar setCalendar(int year, int month, int day) {
+		Calendar c = Calendar.getInstance();
+		c.set(year, month, day);
+		return c;
+	}
+
+	/**
+	 * Convert to String from given date with format dd/MM/yyyy HH:mm:ss EEEE
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static String toString(Date date) {
+		Objects.requireNonNull(date);
+		return dtf.format(date);
 	}
 }
