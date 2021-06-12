2eff0b02df18631556b38681bc9a614bd2069b44
QuocHung <tranquochung381@gmail.com>
2021-06-12 22:24:23 +0700

    lesson07-libraries 12.06.2021

---
+++ lesson07-library/src/datetime/Ex04.java
@@ -0,0 +1,40 @@
+package datetime;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import utils.DateUtils;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		// Epoch
+		Calendar born = DateUtils.getCalendar(2021, Calendar.JUNE, 5);
+		born.set(Calendar.HOUR_OF_DAY, 20);
+
+		Calendar today = Calendar.getInstance();
+
+		if (born.after(today)) {
+			throw new RuntimeException("born cannor greated than today !");
+		}
+
+		long bornInMils = born.getTimeInMillis();
+		long todayInMils = today.getTimeInMillis();
+
+		// millisecons => days, hours, minutes, seconds, milliseconds
+		long duration = todayInMils - bornInMils;
+
+		long days = TimeUnit.MILLISECONDS.toDays(duration);
+		duration -= TimeUnit.DAYS.toMillis(days);
+
+		long hours = TimeUnit.MILLISECONDS.toMillis(duration);
+		duration -= TimeUnit.HOURS.toMillis(hours);
+
+		long minutes = TimeUnit.MILLISECONDS.toMillis(duration);
+		duration -= TimeUnit.MINUTES.toMillis(minutes);
+
+		long seconds = TimeUnit.MILLISECONDS.toMillis(duration);
+		duration -= TimeUnit.SECONDS.toMillis(seconds);
+
+		System.out.println(days + " D " + hours + " H " + minutes + " M " + seconds + " S " + duration + " MS ");
+	}
+}
--- lesson07-library/src/utils/DateUtils.java
+++ lesson07-library/src/utils/DateUtils.java
@@ -1,15 +1,102 @@
 package utils;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class DateUtils {
+	private static final String DATE_PATTERN = "dd/MM/yyy";
+	private static final String DATE_TIME_PATTERN = "dd/MM/yyy HH:mm:ss EEEE";
+
+	private static String[] weekdays = { "Sun", "Mon", " Tue", "Wed", "Thur", "Fri", "Sat" };
 	private static GregorianCalendar gc = new GregorianCalendar();
-	
+	private static DateFormat df = new SimpleDateFormat(DATE_PATTERN);
+	private static DateFormat dtf = new SimpleDateFormat(DATE_TIME_PATTERN);
+
 	private DateUtils() {
-		
+
 	}
-	
+
 	public static boolean isLeapYear(int year) {
 		return gc.isLeapYear(year);
+	}
+
+	public static String getWeekDayAsString(int weekDayAsNumber) {
+		return weekdays[weekDayAsNumber - 1];
+	}
+
+	/**
+	 * Convert to Date from given text with format dd/MM/yyyy.
+	 * 
+	 * @param text input
+	 * @return converted Date
+	 */
+
+	public static Date toDate(String text) {
+		if (text == null || text.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		Date date = null;
+		try {
+			date = df.parse(text);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
+
+	/**
+	 * Convert to date from give Calendar. E.g: 10.10.1990 >> Epoch time. 01.01.1970
+	 * >> Date, Calendar
+	 * 
+	 * @param c input
+	 * @return converted date
+	 */
+	public static Date toDate(Calendar c) {
+		Objects.requireNonNull(c);
+		return c.getTime();
+	}
+
+	/**
+	 * Convert to Calendar from give date. E.g: 10.10.1990 >> Epoch time. 01.01.1970
+	 * >> Date, Calendar
+	 * 
+	 * @param date input
+	 * @return converted Calendar
+	 */
+	public static Calendar toCalendar(Date date) {
+		Objects.requireNonNull(date);
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		return c;
+	}
+
+	/**
+	 * Get calendar from given year, month, day
+	 * 
+	 * @param year  given year
+	 * @param month given month
+	 * @param day   given day
+	 * @return Calendar
+	 */
+	public static Calendar getCalendar(int year, int month, int day) {
+		Calendar c = Calendar.getInstance();
+		c.set(year, month, day);
+		return c;
+	}
+
+	/**
+	 * Convert to String from give date with format dd/MM/yyy HH:mm:ss EEEE
+	 * 
+	 * @param date
+	 * @return converted String
+	 */
+	public static String toString(Date date) {
+		Objects.requireNonNull(date);
+		return dtf.format(date);
 	}
 }
