bfb329f0a7ac4dd277a66cdce7153274e41acc89
dtran <trananhdao276@gmail.com>
2021-06-13 12:38:24 +0700

    lesson07-librabries 12.06.2021

--- lesson07-librabries/src/datetime/Ex04.java
+++ lesson07-librabries/src/datetime/Ex04.java
@@ -1,5 +1,47 @@
 package datetime;
 
-public class Ex04 {
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
+import utils.DateUtils;
+
+public class Ex04 {
+	
+	
+		public static void main(String[] args) {
+			// Epoch
+			Calendar born = DateUtils.getCalendar(2003, Calendar.DECEMBER, 23);
+			born.set(Calendar.HOUR_OF_DAY, 20);
+			
+			Calendar today = Calendar.getInstance();
+
+			if (born.after(today)) {
+				throw new RuntimeException("born cannot greated than today!");
+			}
+
+			long bornInMils = born.getTimeInMillis();
+			long todayInMils = today.getTimeInMillis();
+
+			// milliseconds => day
+			long duration = todayInMils - bornInMils;
+
+			long days = TimeUnit.MILLISECONDS.toDays(duration);
+			duration -= TimeUnit.DAYS.toMillis(days);
+
+			long hours = TimeUnit.MILLISECONDS.toHours(duration);
+			duration -= TimeUnit.HOURS.toMillis(hours);
+
+			long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
+			duration -= TimeUnit.MINUTES.toMillis(minutes);
+
+			long seconds = TimeUnit.MILLISECONDS.toSeconds(duration);
+			duration -= TimeUnit.SECONDS.toMillis(seconds);
+
+			System.out.println(days    + " D " 
+							+ hours    + " H " 
+							+ minutes  + " M " 
+							+ seconds  + " S "
+							+ duration + " MS "
+			);
+		}
 }
