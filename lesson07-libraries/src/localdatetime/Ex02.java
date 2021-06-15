26a6db2c76c2ec917c58a9724b37ad0eaf71c78b
nhanndp <phuocnhan1994@gmail.com>
2021-06-15 22:01:45 +0700

    lesson07-libraries 15.06.2021

---
+++ lesson07-libraries/src/localdatetime/Ex02.java
@@ -0,0 +1,32 @@
+package localdatetime;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Demo with local time
+ * Pattern: Builder Pattern
+ */
+public class Ex02 {
+	
+	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("KK:mm:ss a");
+	public static void main(String[] args) {
+		LocalTime time = LocalTime.now().withHour(21);
+		System.out.println("time: " + time);
+
+		
+		LocalTime anotherTime = LocalTime.of(22, 22, 44);
+		System.out.println("anotherTime: " + anotherTime);
+		
+		System.out.println("formatted: " + dtf.format(time));
+		
+		Duration duration = Duration.between(time, anotherTime);
+		System.out.println("duration: " + duration);
+		System.out.println("toString: " + duration.toHoursPart() + "H"
+										+ duration.toMinutesPart() + "M"
+										+ duration.getSeconds() + "S");
+
+	}
+
+}
---
+++ lesson07-libraries/src/localdatetime/Ex03.java
@@ -0,0 +1,61 @@
+package localdatetime;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Ex03 {
+
+	private static String LocalizedKey = "dd.MM.yyyy'T'KK:mm:ss a";
+	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern(LocalizedKey);
+
+	public static void main(String[] args) {
+		LocalDateTime datetime = LocalDateTime.now();
+		System.out.println("datetime: " + datetime);
+
+		LocalDateTime anotherLocalDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+		System.out.println("anotherLocalDateTime: " + anotherLocalDateTime);
+
+		System.out.println("formatted: " + dtf.format(datetime));
+
+		// Period: LocalDate
+		// Duration: LocalTime
+		// : LocalDatetime
+
+		LocalDateTime dtStart = LocalDateTime.of(1995, 9, 25, 10, 0, 0);
+		LocalDateTime dtEnd   = LocalDateTime.now();
+		
+		String duration = between(dtStart, dtEnd);
+		System.out.println("duration: " + duration);
+	}
+
+	private static String between(LocalDateTime dtStart, LocalDateTime dtEnd) {
+		Objects.requireNonNull(dtStart);
+		Objects.requireNonNull(dtEnd);
+		
+		if(dtStart.isAfter(dtEnd)) {
+			throw new IllegalArgumentException("StartDate cannot exceed EndDate !!!");
+		}
+		
+		LocalDate dateStart = dtStart.toLocalDate();
+		LocalDate dateEnd = dtEnd.toLocalDate();
+
+		LocalTime timeStart = dtStart.toLocalTime();
+		LocalTime timeEnd = dtEnd.toLocalTime();
+
+		Period period = Period.between(dateStart, dateEnd);
+		Duration duration = Duration.between(timeStart, timeEnd);
+		
+		if(timeEnd.isBefore(timeEnd)) {
+			duration = duration.plusDays(1);
+			period = period.minusDays(1);
+		}
+
+		return period.toString() + '-' + duration.toString();
+	}
+
+}
