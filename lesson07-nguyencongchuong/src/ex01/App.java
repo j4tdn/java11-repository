734690b6f0c88a76c38b1412f2cd18e83282457d
ncchuong <ncchuong99@gmail.com>
2021-06-16 22:11:43 +0700

    lesson07-nguyencongchuong

--- lesson07-nguyencongchuong/src/ex01/App.java
+++ lesson07-nguyencongchuong/src/ex01/App.java
@@ -7,7 +7,7 @@ import utils.DateUtils;
 public class App {
 	public static void main(String[] args) {
 		getAllSundaysInYear(2021);
-
+		getLastSunday(2021);
 	}
 
 	private static void getAllSundaysInYear(int year) {
@@ -21,6 +21,19 @@ public class App {
 				System.out.println(DateUtils.toString(dayStart.getTime()));
 			}
 			dayStart.add(Calendar.DAY_OF_YEAR, 1);
+		}
+	}
+	
+	private static void getLastSunday(int year) {
+		Calendar dayStart = DateUtils.getCalendar(year, 11, 24);
+		Calendar dayEnd = DateUtils.getCalendar(year, 11, 31);
+		
+		while (!dayStart.after(dayEnd)) {
+			int tmp = dayEnd.get(Calendar.DAY_OF_WEEK);
+			if(tmp == Calendar.SUNDAY) {
+				System.out.println("Last Sunday of the year: " + DateUtils.toString(dayEnd.getTime()));
+			}
+			dayEnd.add(Calendar.DAY_OF_WEEK, -1);
 		}
 	}
 }
