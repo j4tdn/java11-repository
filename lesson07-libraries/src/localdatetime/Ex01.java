580ab30ac88f110f00f44e88bf56822343a74ee2
Hai Dang <haidv245@gmail.com>
2021-06-16 12:55:31 +0700

    lesson07-libraries 16.06.2021 update

--- lesson07-libraries/src/localdatetime/Ex01.java
+++ lesson07-libraries/src/localdatetime/Ex01.java
@@ -6,27 +6,23 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 /**
- * Demo with Local Date
- * Pattern: Builder Pattern
+ * Demo with Local Date Pattern: Builder Pattern
  *
  */
 public class Ex01 {
 	public static void main(String[] args) {
 		LocalDate date = LocalDate.now().withMonth(8);
 		System.out.println("date: " + date);
-		
+
 		LocalDate anotherDate = LocalDate.of(2022, Month.OCTOBER, 22);
 		anotherDate = anotherDate.plusWeeks(2);
 		System.out.println("anotherDate: " + anotherDate);
-		
+
 		Period period = Period.between(date, anotherDate);
 		System.out.println("period: " + period);
-		System.out.println("toString: " + period.getYears() + "Y"
-					+ period.getMonths() + "M"
-					+ period.getDays() + "D"
-				);
-		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-				
+		System.out.println("toString: " + period.getYears() + "Y" + period.getMonths() + "M" + period.getDays() + "D");
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println("formatted: " + dtf.format(date));
 	}
 }
--- lesson07-libraries/src/localdatetime/Ex02.java
+++ lesson07-libraries/src/localdatetime/Ex02.java
@@ -6,22 +6,21 @@ import java.time.format.DateTimeFormatter;
 
 public class Ex02 {
 	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("KK:mm:ss a");
+
 	public static void main(String[] args) {
 		LocalTime time = LocalTime.now().withHour(21);
 		System.out.println("time: " + time);
-		
+
 		LocalTime anotherTime = LocalTime.of(22, 22, 44);
 		System.out.println("anotherTime: " + anotherTime);
-		
+
 		System.out.println("formatted: " + dtf.format(time));
-		
+
 		Duration duration = Duration.between(time, anotherTime);
-		
+
 		System.out.println("duration: " + duration);
-		
-		System.out.println("toString: " + duration.toHoursPart() + "H"
-				+ duration.toMillisPart() + "M"
-				+ duration.toSecondsPart() + "S"
-				);
+
+		System.out.println("toString: " + duration.toHoursPart() + "H" + duration.toMinutesPart() + "M"
+				+ duration.toSecondsPart() + "S");
 	}
 }
