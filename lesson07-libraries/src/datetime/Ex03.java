487fec30a867419492c1bd1c78252864c7ec0e95
vhuy <vanhuy0001dtu@gmail.com>
2021-06-16 15:01:08 +0700

    lesson07-libraries 16.06.2021

--- lesson07-libraries/src/datetime/Ex03.java
+++ lesson07-libraries/src/datetime/Ex03.java
@@ -6,33 +6,33 @@ import utils.DateUtils;
 
 public class Ex03 {
 	public static void main(String[] args) {
-		int[] weekdays = { 1, 2, 3, 4, 5, 6, 7 };
-
-		for (int weekday : weekdays) {
-			String wdAsString = DateUtils.getWeekdayAsString(weekday);
-			System.out.println("number of: " + wdAsString + nofWeekDays(6, 2021, weekday));
-		}
-	}
-
-	// month = 6
-	// weekday = 1 (Sunday) 2(Monday)
-	private static int nofWeekDays(int month, int year, int weekday) {
-		Calendar dayStart = Calendar.getInstance();
-		dayStart.set(year, weekday, 1);
-
-		Calendar dayEnd = Calendar.getInstance();
-		dayEnd.set(year, month, dayStart.getActualMaximum(Calendar.DAY_OF_MONTH));
-
-		int count = 0;
-		while (!dayStart.after(dayEnd)) {
-			int tmp = dayStart.get(Calendar.DAY_OF_WEEK);
-			if (tmp == weekday) {
-				count++;
-				dayStart.add(Calendar.DAY_OF_MONTH, 7);
-			} else {
-				dayStart.add(Calendar.DAY_OF_MONTH, 1);
+		// Count number of SUNDAYs in this month
+				int[] weekdays = {1,2,3,4,5,6,7};
+				for(int weekday : weekdays) {
+					System.out.println("number of " + DateUtils.getWeekdayAsString(weekday) + ":" + nofWeekDay(5, 2021, weekday));
+				}
 			}
-			return count;
+			
+			private static int nofWeekDay(int month, int year, int weekday) {
+				Calendar dayStart = Calendar.getInstance();
+				dayStart.set(year,  month, 1);
+				
+				Calendar dayEnd = Calendar.getInstance();
+				dayEnd.set(year, month, dayStart.getActualMaximum(Calendar.DAY_OF_MONTH));
+//				System.out.println("day start: " + DateUtils.toString(dayStart.getTime()));
+//				System.out.println("day end: " + DateUtils.toString(dayEnd.getTime()));
+				
+				int count = 0;
+				while(!dayStart.after(dayEnd)) {
+//					System.out.println(DateUtils.toString(dayStart.getTime()));
+					int tmp = dayStart.get(Calendar.DAY_OF_WEEK);
+					if(tmp == weekday) {
+						count ++;
+						dayStart.add(Calendar.DAY_OF_MONTH, 7);
+					}else {
+						dayStart.add(Calendar.DAY_OF_MONTH, 1);
+					}
+				}
+				return count;
+			}
 		}
-	}
-}
--- lesson07-libraries/src/datetime/Ex04.java
+++ lesson07-libraries/src/datetime/Ex04.java
@@ -7,36 +7,40 @@ import utils.DateUtils;
 
 public class Ex04 {
 	public static void main(String[] args) {
-		Calendar born = DateUtils.getCalendar(2021, Calendar.JUNE, 05);
-		born.set(Calendar.HOUR_OF_DAY, 20);
-		
-		Calendar today = Calendar.getInstance();
-		
-		if(born.after(today)) {
-			throw new RuntimeException("born can't greated than today");
+		public static void main(String[] args) {
+			// Epoch
+			Calendar born = DateUtils.setCalendar(1999, Calendar.MAY, 24);
+			born.set(Calendar.HOUR_OF_DAY, 20);
+			Calendar today = Calendar.getInstance();
+			
+			if(born.after(today)) {
+				throw new RuntimeException("born cannot greated than today!");
+			}
+			
+			long startInMils = born.getTimeInMillis();
+			long todayInMils = today.getTimeInMillis();
+			
+			// milliseconds => days, hours, minutes, seconds, milliseconds
+			long duration = todayInMils - startInMils;
+//			System.out.println(duration);
+			
+			long days = TimeUnit.MILLISECONDS.toDays(duration);
+			duration = duration - TimeUnit.DAYS.toMillis(days);
+			
+			long hours = TimeUnit.MILLISECONDS.toHours(duration);
+			duration = duration - TimeUnit.HOURS.toMillis(hours);
+			
+			long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
+			duration = duration - TimeUnit.MINUTES.toMillis(minutes);
+
+			long seconds = TimeUnit.MILLISECONDS.toSeconds(duration);
+			duration = duration - TimeUnit.MINUTES.toMillis(seconds);
+			
+			System.out.println(days + "D "
+						+ hours + "H "
+						+ minutes + "M "
+						+ seconds + "S "
+						+ duration + "MS"
+					);
 		}
-		long bornInMils = born.getTimeInMillis();
-		long todayInMils =  today.getTimeInMillis();
-		
-		//miliseconds => days, hours, mins, secs, milis
-		long duration = todayInMils - bornInMils; 
-		
-		long days = TimeUnit.MILLISECONDS.toDays(duration);
-		duration -= TimeUnit.DAYS.toMillis(days);
-		
-		long hours = TimeUnit.MILLISECONDS.toHours(duration);
-		duration -= TimeUnit.HOURS.toMillis(hours);
-		
-		long minutes = TimeUnit.MILLISECONDS.toHours(duration);
-		duration -= TimeUnit.MINUTES.toMillis(minutes);
-		
-		long seconds = TimeUnit.MILLISECONDS.toHours(duration);
-		duration -= TimeUnit.SECONDS.toMillis(seconds);
-		
-		System.out.println(days + " Days"
-						+ hours + " Hours"
-						+ minutes + " Minutes"
-						+ seconds + " Seconds"
-				);
 	}
-}
