0f38d2bcc812f81121e7a8b7cd41d66c2d41c56b
huongmy <huongmydn@gmail.com>
2021-06-19 22:27:34 +0700

    lesson08-string 19.06.2021

---
+++ lesson07-libraries/src/number/format/Ex02.java
@@ -0,0 +1,15 @@
+package number.format;
+
+import java.text.DecimalFormat;
+
+public class Ex02 {
+	private static final String PATTERN ="#,###.00";
+	
+	public static void main(String[] args) {
+		double number = 12357612.2346;
+		DecimalFormat fm = new DecimalFormat(PATTERN);
+		
+		System.out.println("value: " + fm.format(number));
+		
+	}
+}
