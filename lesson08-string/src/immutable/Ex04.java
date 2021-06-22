f032d0bf9b87dfab6f2b8889c2ee1e6c68e31667
QuocHung <tranquochung381@gmail.com>
2021-06-22 22:50:50 +0700

    lesson08-string 22/06/2021

---
+++ lesson08-string/src/immutable/Ex04.java
@@ -0,0 +1,44 @@
+package immutable;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		String[] books = {"SGK123", "STK234", "SGK223"};
+		
+		// Get classical book
+		for (String book: books) {
+			if (book.startsWith("STK")) {
+				System.out.println(book);
+			}
+		}
+		System.out.println("============");
+		String layout = "GridLayout";
+		layout = layout.replace("Grid", "Border");
+		System.out.println(layout);
+		
+		String hobbies = "Football,,,,,    Volleyball,,,   Badminton";
+		hobbies = hobbies.replaceAll("[,\\s]+", " ");
+		System.out.println("hobbies: " + hobbies);
+		
+		String parent = "abcdefghklm";
+		CharSequence sub = parent.substring(2, 7);
+		System.out.println(sub);
+		
+		char[] chars = parent.toCharArray();
+		System.out.println("i6: " + chars[6]);
+		System.out.println("c6: " + parent.charAt(6));
+		
+		String[] employee = {"EMP01", "Adma", " 2500", "Newyork"};
+		String result = "";
+		for (String attribute: employee) {
+			result += "-" + attribute;
+		}
+		System.out.println("result 1: " + result);
+		result = String.join("-", "EMP01", "Adma", " 2500", "Newyork");
+		System.out.println("result 2: " + result);
+		
+	}
+	
+	// ->.... ..::
+	// variables ...
+	// ... stands for array || set of 1 or n elements
+}
---
+++ lesson08-string/src/immutable/Ex05.java
@@ -0,0 +1,27 @@
+package immutable;
+
+import bean.Student;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		// student.txt
+		// 1. Join, 094 329 2323, Male, English-Math
+		// 2. July, 034 098 2323, Female, Biology-Chemistry
+		
+		// class: Student >> name, phone, gender, hobbies
+		
+		String line = "Join, 094 329 2323, Male, English-Math";
+		Student student = transfomer(line);
+		System.out.println(student);
+	}
+	
+	private static Student transfomer(String line) {
+		Student student = null;
+		String [] elements = line.split(",");
+		System.out.println("============");
+		if (elements.length == 4) {
+			student = new Student(elements[0], elements[1], "male".equalsIgnoreCase(elements[2]), elements[3]);
+		}
+		return student;
+	}
+}
