302ca73f1bc7e4de4fb38aab06cc0dd00e8a974e
HoangSon <hthaison17@gmail.com>
2021-06-26 20:09:00 +0700

    lesson08-hthaison 26/6/2021

---
+++ lesson08-hoangthaison/src/ex01/App.java
@@ -0,0 +1,12 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập Chuỗi");
+		String text = sc.nextLine();
+	}
+
+}
