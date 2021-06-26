6d27752e5c48ad05dccb493af4163c61bf29de9a
trungvo <trung16vlxd@gmail.com>
2021-06-26 22:23:28 +0700

    lesson09-trungvo 26.06.2021

---
+++ lesson09-trungvo/src/Ex01/StudentPoint.java
@@ -0,0 +1,14 @@
+package Ex01;
+
+public class StudentPoint {
+	public static void main(String[] args) {
+		//Student student = new Student();
+		
+		Student[] students = new Student[10];
+		
+		
+		
+	}
+	
+	
+}
