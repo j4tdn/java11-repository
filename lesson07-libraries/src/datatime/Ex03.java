7c78eac5542256895479a75c14ee5fb940cbe731
Thai Luc <thaitangluc2412@gmail.com>
2021-06-28 01:17:00 +0700

    lesson07-libraries 28.06.2021

---
+++ lesson07-libraries/src/datatime/Ex03.java
@@ -0,0 +1,44 @@
+ package datatime;
+
+import java.util.Calendar;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		
+		// Print all current weekdays
+		Calendar c = Calendar.getInstance();// 24.06.2021
+		c.set(Calendar.MONTH, Calendar.FEBRUARY);
+		c.set(Calendar.YEAR, 2020);
+		
+		System.out.println("Year ActualMaxDays: " + c.getActualMaximum(Calendar.DAY_OF_YEAR));
+		System.out.println("ActualMaxDays: " + c.getActualMaximum(Calendar.DAY_OF_MONTH));
+		System.out.println("MaxDays: " + c.getMaximum(Calendar.DAY_OF_MONTH));
+		
+		// get first day of week
+		c.add(Calendar.DAY_OF_MONTH, c.getFirstDayOfWeek() - c.get(Calendar.DAY_OF_WEEK));
+		System.out.println("=========================");
+		printWeekdays(c);
+		printDaysInMonth(c);
+		
+	}
+	
+	private static void printWeekdays(Calendar c) {
+		int day = 1;
+		while(day <= 7) {
+			System.out.println(c); // dd/MM//yyyy HH:mm:ss
+			c.add(Calendar.DAY_OF_MONTH, 1);
+			day++;
+		}
+	}
+	
+	private static void printDaysInMonth(Calendar c) {
+		int daysInMonth = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+		c.getMaximum(Calendar.DAY_OF_MONTH);
+		int day = 1;
+		while(day <= daysInMonth) {
+			System.out.println(c);
+			c.add(Calendar.DAY_OF_MONTH, 1);
+			day++;
+		}
+	}
+}
--- lesson07-libraries/src/utils/DateUtils.java
+++ lesson07-libraries/src/utils/DateUtils.java
@@ -1,8 +1,13 @@
 package utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class DateUtils {
+	private static final String ddMMyyyy = "dd/MM/yyyy";
 	private static final GregorianCalendar gc = new GregorianCalendar();
 	
 	private DateUtils() {
@@ -11,5 +16,18 @@ public class DateUtils {
 	
 	public static boolean isLeapYear(int year) {
 		return gc.isLeapYear(year);
+	}
+	
+	/**
+	 * Convert Calendar to formated string with dd/MM/yyyy pattern
+	 * 
+	 * @param c given calendar
+	 * @returnformated calendar as string
+	 */
+	public static String format(Calendar c) {
+		Objects.requireNonNull(c);
+		
+		DateFormat df = new SimpleDateFormat(ddMMyyyy);
+	return df.formar(c.getTime());
 	}
 }
