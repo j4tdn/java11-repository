818e2cada726aa6c90da3cacb96cf68f1e4fe53f
nuhigin <nuhigin@gmail.com>
2021-07-10 16:52:41 +0700

    lesson10-array array-manipulation 06.07.2021

--- lesson10-array/src/manipulation/Ex02.java
+++ lesson10-array/src/manipulation/Ex02.java
@@ -1,5 +1,20 @@
 package manipulation;
 
 public class Ex02 {
+	public static void main(String[] args) {
+		int[] digits = {1, 2, 3, 4, 5};
+		reverse(digits);
+		for(int i: digits) {
+			System.out.print(i+", ");
+		}
+	}
+	
+	private static void reverse(int[] digits) {
+		for(int i = 0; i < digits.length/2; i++) {
+			int tmp = digits[i];
+			digits[i] = digits[digits.length - i -1];
+			digits[digits.length - i - 1] = tmp;
+		}		
+	}
 
 }
