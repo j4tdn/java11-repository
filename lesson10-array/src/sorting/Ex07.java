7247a838204326c09f22a6b348468e8c3fd8c5cf
ncchuong <ncchuong99@gmail.com>
2021-07-20 22:23:57 +0700

    lesson10-array 20.07.2021

--- lesson10-array/src/sorting/Ex07.java
+++ lesson10-array/src/sorting/Ex07.java
@@ -1,33 +1,52 @@
 package sorting;
 
-import static java.util.Comparator.comparing;
-import static java.util.Comparator.reverseOrder;
-
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.function.Function;
 
+import static java.util.Comparator.*;
 import bean.Item;
 import utils.ArrayUtils;
 
 public class Ex07 {
 	public static void main(String[] args) {
-		Item[] items = getItem();
+		Item[] items = getItems();
 		
+		// lambda expression
+		// Function<T, R> >> t -> t.X();
+		// t: instance of T
+		// X: T's method return R
 		
-		Arrays.sort(items, comparing(i -> i.getName()));
+		Item itemX = new Item(111, "AAA", 333);
 		
+		Function<Item, String> f1 = (Item item) -> item.getName();
+		System.out.println("f1: " + f1.apply(itemX));
+		
+		// method reference
+		Function<Item, Integer> f2 = Item::getItemId;
+		System.out.println("f2: " + f2.apply(itemX));
+		
+		// Arrays.sort(items, comparing(i -> i.getName()));
+		Arrays.sort(items, comparing(Item::getName));
 		ArrayUtils.printf(items);
-		System.out.println("===============");
-		Comparator<Item> comparator = comparing(i -> i.getStoreId());
-		Arrays.sort(items, comparator.thenComparing(i -> i.getItemId()).reversed());
+		
+		System.out.println("======================");
+		
+		Function<Item, Integer> f3 = Item::getStoreId;
+		System.out.println("f3: " + f3.apply(itemX));
+		
+		Comparator<Item> comparator = comparing(Item::getStoreId, reverseOrder());
+		Arrays.sort(items, comparator.thenComparing(Item::getItemId).reversed());
 		ArrayUtils.printf(items);
-}
+		
+		// . ; :: ... -> () {} []
+	}
 	
-	private static Item[] getItem() {
+	private static Item[] getItems() {
 		return new Item[] {
-			new Item(1, 12, "a", 278),
-			new Item(2, 27, "B", 12),
-			new Item(3, 12, "c", 278),
+			new Item(1, 12, "a", 278.2d),
+			new Item(2, 27, "B", 12.2d),
+			new Item(3, 12, "c", 228),
 			new Item(2, 29, "D", 259),
 			new Item(1, 18, "f", 278),
 			new Item(1, 10, "K", 159),
