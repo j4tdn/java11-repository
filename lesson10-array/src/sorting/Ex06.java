8610fdeff3f78d1e1266b8c1c1841789181845cc
dtran <trananhdao276@gmail.com>
2021-07-20 22:28:55 +0700

    lesson10-array 20.07.2021

--- lesson10-array/src/sorting/Ex06.java
+++ lesson10-array/src/sorting/Ex06.java
@@ -7,39 +7,55 @@ import utils.ArrayUtils;
 
 public class Ex06 {
 	public static void main(String[] args) {
-		String[] sequences = { "a", null, "d", "c", "b", null, "e"};
-
-		// Not handle null elements
-		// Arrays.sort(sequences); >> Comparable
-
-		Arrays.sort(sequences, new Comparator<String>() {
+		String[] sequences = { "a", null, "d", "c", "b", null, "e" };
+		// null null "" "" >> null first
+		// "" "" null null >> null last
+		
+		Comparator<String> comparator = new Comparator<String>() {
 
 			@Override
 			public int compare(String o1, String o2) {
-				// ascending: o1(left) o2(right) of pivot
-				// o1: previous
-				// o2: next
-				// o1 X o2
-				// compare(o1, o2) = X as below
-				// positive: o1 > o2 (default)
-				// zero	   : o1 == o2
-				// negative: o1 < o2
-
-				// null first
-				if(o1 == null) {
-					return -1; // o1 > o2 swap(o1,o2)
+				// o1: previous element
+				// o2: next elements
+				// compare(o1, o2) > 0 ==> swap(o1, o2)
+				
+				// handle null >> null first, null last
+				// null first: null elements always less than non-null elements
+				// null last : null elements always greater than non-null elements
+				
+				// Requirement: NULL FIRST
+				if (o1 == null) {
+					return -1; // o1=null >> o1<o2
 				}
+				
 				// o1 != null
-				if(o2 == null) {
-					return 1; // o2 < o1
+				if (o2 == null) {
+					return 1; // o2=null >> o1>o2
 				}
-
-				// ascending
+				
+				// o1!=null
+				// o2!=null
 				return o1.compareTo(o2);
 			}
-		});
-
+		};
+		
+		// Comparator
+		Arrays.sort(sequences, comparator);
 		ArrayUtils.printf(sequences);
+		
+		
+		// Comparable
+		
+		// X[] sequences
+		// Required: X implements Comparable<X>
+		// >> incompatible with java.lang.Comparable
+		// Arrays.sort(sequences); 
+		// ArrayUtils.printf(sequences);
+		
+		// Tuple no implementation from Comparable
+		// Tuple[] tuples = {new Tuple(5, 2), new Tuple(2,3)};
+		// Arrays.sort(tuples); 
+		// System.out.println(tuples);
 	}
 
 }
---
+++ lesson10-array/src/sorting/Ex061.java
@@ -0,0 +1,28 @@
+package sorting;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import utils.ArrayUtils;
+import static java.util.Comparator.*;
+import static java.util.function.Function.*;
+public class Ex061 {
+	public static void main(String[] args) {
+String[] sequences = { "a", null, "d", "c", "b", null, "e" };
+		
+		// lambda expression: instance of functional interface
+		Comparator<String> precomparator = (s1, s2) -> s1.compareTo(s2);
+		
+		
+		// Function<T, R> >> keyExtractor >> abstract method >> R apply(T t);
+// body
+		// (c1, c2) -> keyExtractor.apply(c1).compareTo(keyExtractor.apply(c2))
+		
+		// available method similar to "(s1, s2) -> s1.compareTo(s2)"
+		Comparator<String> comparator = nullsLast(comparing(identity(), reverseOrder()));
+		
+		Arrays.sort(sequences, comparator);
+		ArrayUtils.printf(sequences);
+	}
+
+}
