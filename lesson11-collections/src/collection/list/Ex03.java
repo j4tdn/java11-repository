6cd0ef198e0522f1c9dda8f245e5d24b64e169b9
ncchuong <ncchuong99@gmail.com>
2021-07-24 22:39:06 +0700

    lesson11-collections 24.07.2021

---
+++ lesson11-collections/src/collection/list/Ex03.java
@@ -0,0 +1,46 @@
+package collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		// String[] elements = new String[0];
+		// elements.get(0) = elements[0];
+		
+		// Constructs an empty list with an initial capacity of ten.
+		// this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+		// DEFAULT_CAPACITY = 10;
+		
+		List<String> elements = new ArrayList<>(8);
+		
+		// modCount++;
+        // add(e, elementData, size);
+        // return true;
+		
+		elements.add("Water");
+		elements.add("Bottle");
+		elements.add(0, "Paper");
+		
+		elements.set(1, "Can");
+		
+		// remove index
+		elements.remove(0);
+		
+		// remove value
+		elements.remove("Can");
+		
+		// loop
+		for (int i = 0; i < elements.size(); i++) {
+			System.out.println("element " + i + ": " + elements.get(i));
+		}
+		
+		// System.out.println("element 0: " + elements.get(0));
+		// System.out.println("element 1: " + elements.get(1));
+		// System.out.println("element 2: " + elements.get(2));
+		
+		System.out.println("size: " + elements.size());
+		System.out.println("isEmpty: " + elements.isEmpty());
+
+	}
+}
