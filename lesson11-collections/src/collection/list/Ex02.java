750285c2c8d212d51a6b0bcaa1b1cd8a9631c355
hpham <binhmt2008@gmail.com>
2021-07-24 22:39:34 +0700

    lesson11-collections 24.07.2021

---
+++ lesson11-collections/src/collection/list/Ex02.java
@@ -0,0 +1,21 @@
+package collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		// Generic type: >=1.5
+		// Catch error at compile time
+		
+		List<Integer> digits = new ArrayList<>();
+		List<String> sequences = new ArrayList<>();
+		
+		digits.add(123);
+		digits.add(234);
+
+		for (Integer digit : digits) {
+			System.out.println("double: " + digit * 2);
+		}
+	}
+}
---
+++ lesson11-collections/src/collection/list/Ex04.java
@@ -0,0 +1,48 @@
+package collection.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		List<String> elements = new ArrayList<>();
+		elements.add("Item - 5");
+		elements.add("Item - 1");
+		elements.add("Item - 2");
+		elements.add("Item - 0");
+		
+		elements.removeIf(item -> Integer.parseInt("" + item.charAt(item.length() - 1)) <= 3);			
+		
+		System.out.println("contains: " + elements.contains(("Item - 5")));
+		
+		System.out.println("=============");
+		for(String element: elements) {
+			System.out.println(element);
+		}
+		
+		/*Iterator<String> iterator = elements.iterator();
+		while(iterator.hasNext()) {
+			String item = iterator.next();
+			int remainingDays = Integer.parseInt("" + item.charAt(item.length() - 1));
+
+			if (remainingDays <= 3) {
+				System.out.println("removed element: " + item);
+				iterator.remove();
+			}
+		}*/
+		
+		/*for (int i = 0; i < elements.size(); i++) {
+			String item = elements.get(i);
+			int remainingDays = Integer.parseInt("" + item.charAt(item.length() - 1));
+
+			if (remainingDays <= 3) {
+				System.out.println("removed element: " + item);
+				//elements.remove(i);
+				elements.remove(item);
+				i--;
+			}
+		}*/
+	}
+}
