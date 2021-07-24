ea7b5e0f7e0eca147509028461a77b67b1735b49
vhuy <vanhuy0001dtu@gmail.com>
2021-07-24 22:43:58 +0700

    lesson11-collection 24.07.2021

---
+++ lesson11-collections/src/collection/list/Ex01.java
@@ -0,0 +1,25 @@
+package collection.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		//prior to 1.5
+		List l1 = new ArrayList();
+		List l2 = new LinkedList();
+		List l3 = new Stack();
+		
+		//
+		l1.add("element");
+		l1.add(new Integer(10));
+		
+		System.out.println("l1 size: " + l1.size());
+		
+		for (int i = 0; i < l1.size(); i++) {
+			System.out.println(i +  ": " + Integer.parseInt(l1.get(i).toString()));
+		}
+	}
+}
