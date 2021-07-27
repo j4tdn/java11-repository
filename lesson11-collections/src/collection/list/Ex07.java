1e9e8d4a8f6bf54603ab5287338fbd411640f650
cuong <ngocuong12092001@gmail.com>
2021-07-27 22:57:33 +0700

    lesson11-collections 27.07.2021

---
+++ lesson11-collections/src/collection/list/Ex07.java
@@ -0,0 +1,46 @@
+package collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Item;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		
+		List<Item> items = getItems();
+
+		Item itemA = new Item(1, 2, "A12", 12);
+		Item itemB = new Item(1, 10, "A10", 100);
+		Item itemC = items.get(2);
+		
+		// Implementation note: List<E> elements
+		// Need call: remove, contains
+		// Require: E override equals methods
+		// Without: using default Object's equals to comparing address in heap memory
+		
+		System.out.println("itemA exists > " + items.contains(itemA)); // false
+		System.out.println("itemB exists > " + items.contains(itemB)); // true
+		System.out.println("itemC exists > " + items.contains(itemC)); // true
+		
+		items.remove(itemB);
+		
+		System.out.println("size: " + items.size());
+	}
+
+	private static List<Item> getItems() {
+		List<Item> items = new ArrayList<>();
+
+		Item item1 = new Item(1, 10, "A10", 100);
+		Item item2 = new Item(2, 12, "A20", 200);
+		Item item3 = new Item(3, 30, "A30", 300);
+		Item item4 = new Item(1, 12, "A12", 120);
+
+		items.add(item1);
+		items.add(item2);
+		items.add(item3);
+		items.add(item4);
+
+		return items;
+	}
+}
---
+++ lesson11-collections/src/utils/CollectionUtils.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionUtils {
+	private CollectionUtils() {
+	}
+
+	public static void printf(List<String> elements) {
+		for (String element : elements) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+
+	public static void show(List<Integer> elements) {
+		for (Integer element : elements) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+
+	public static List<Integer> init(Integer... elements) {
+		List<Integer> result = new ArrayList<>();
+		result.addAll(Arrays.asList(elements));
+		return result;
+	}
+}
