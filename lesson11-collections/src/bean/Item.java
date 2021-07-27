c6798721466892834a765a510b88985c27577222
Thai Luc <thaitangluc2412@gmail.com>
2021-07-27 18:20:33 +0700

    lesson11-collections 27.07.2021

---
+++ lesson11-collections/src/bean/Item.java
@@ -0,0 +1,68 @@
+package bean;
+
+public class Item {
+	private int storeId;
+	private int itemId;
+	private String name;
+	private double price;
+
+	public Item() {
+	}
+
+	public Item(int storeId, int itemId, String name, double price) {
+		this.storeId = storeId;
+		this.itemId = itemId;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(int storeId) {
+		this.storeId = storeId;
+	}
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Item)) {
+			return false;
+		}
+		Item that = (Item)o;
+		return getStoreId() == that.getStoreId() && getItemId() == that.getItemId();
+	}
+	
+	@Override
+	public String toString() {
+		return "Item [storeId=" + storeId + ", itemId=" + itemId + ", name=" + name + ", price=" + price + "]";
+	}
+
+}
