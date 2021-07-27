4e78bb0b1c659e7b893c57c0c3323af4093ca36f
QuocHung <tranquochung381@gmail.com>
2021-07-27 22:54:37 +0700

    lesson11-collections 27.07.2021

---
+++ lesson11-collections/src/collection/list/Ex06.java
@@ -0,0 +1,20 @@
+package collection.list;
+
+import java.util.List;
+
+import static utils.CollectionUtils.*;
+
+public class Ex06 {
+	public static void main(String[] args) {
+		List<Integer> first = init(1, 2, 3, 4, 5);
+		List<Integer> second = init(4, 5, 6, 7);
+
+		// addAll: Hợp
+		// removeAll: remove Giao
+		// retainAll: remove Hiệu
+		first.retainAll(second);
+
+		show(first);
+		show(second);
+	}
+}
