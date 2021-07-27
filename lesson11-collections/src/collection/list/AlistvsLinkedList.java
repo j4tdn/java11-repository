2a6c096527bd19201166507d5882ad0b427ecb8b
nhanndp <phuocnhan1994@gmail.com>
2021-07-27 22:56:59 +0700

    lesson11-collections 27-07-2021

---
+++ lesson11-collections/src/collection/list/AlistvsLinkedList.java
@@ -0,0 +1,5 @@
+package collection.list;
+
+public class AlistvsLinkedList {
+
+}
---
+++ lesson11-collections/src/collection/list/Ex07.java
@@ -0,0 +1,42 @@
+package collection.list;
+
+import java.awt.ItemSelectable;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Item;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		List<Item> items = getItems();
+		
+		Item itemA = new Item(1, 2, "A12", 12);
+		Item itemB = new Item(1, 10, "A10", 100);
+		Item itemC = items.get(2);
+		
+		
+		System.out.println("itemA exists > " + items.contains(itemA));
+		System.out.println("itemB exists > " + items.contains(itemB));
+		System.out.println("itemC exists > " + items.contains(itemC));
+		
+		items.remove(itemB);
+		System.out.println("size: " + items.size());
+		
+	}
+	
+	private static List<Item> getItems(){
+		List<Item> items = new ArrayList<>();
+		
+		Item item1 = new Item(1, 10, "A10", 100);
+		Item item2 = new Item(2, 20, "A20", 200);
+		Item item3 = new Item(3, 30, "A30", 300);
+		Item item4 = new Item(1, 12, "A12", 120);
+		
+		items.add(item1);
+		items.add(item2);
+		items.add(item3);
+		items.add(item4);
+		
+		return items;
+	}
+}
