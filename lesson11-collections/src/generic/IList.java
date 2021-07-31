1bf5cd4b8566129c84ec49439123292ad94f58ab
nhanndp <phuocnhan1994@gmail.com>
2021-07-31 23:00:51 +0700

    lesson11-collections 31-07-2021

---
+++ lesson11-collections/src/generic/IList.java
@@ -0,0 +1,14 @@
+ackage generic;
+
+import java.util.function.Predicate;
+
+public interface IList<E> {
+	int size();
+	boolean isEmpty();
+	boolean add(E t);
+	boolean remove(int i);
+	E get(int i);
+	void set (int i, E t);
+	void show();
+	int count(Predicate<E> cond);
+}
