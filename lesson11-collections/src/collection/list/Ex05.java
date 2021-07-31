885ffb5b31fcf27b3432ba6671a1ad52dd9b3507
HoangSon <hthaison17@gmail.com>
2021-07-31 10:35:36 +0700

    lesson11-collections late 27-7-2021

---
+++ lesson11-collections/src/collection/list/Ex05.java
@@ -0,0 +1,21 @@
+package collection.list;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import utils.CollectionUtils;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		// fixed-size
+		List<String> elements = Arrays.asList("a", "b", "c", "d", "e", "f");
+		System.out.println("random: " + elements.get(new Random().nextInt(elements.size())));
+		boolean isExist = elements.contains("b");
+		Collections.reverse(elements);
+		Collections.shuffle(elements);
+		System.out.println("isExist: " + isExist);
+		CollectionUtils.printf(elements);
+	}
+}
