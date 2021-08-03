e4a152925c91d645640c682e2bbdf765c778e1e0
teoven <68175492+teoven@users.noreply.github.com>
2021-08-03 23:06:51 +0700

    lesson11-collection 03.08.2021

---
+++ lesson11-collection/src/collection/map/Ex02.java
@@ -0,0 +1,41 @@
+package collection.map;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		// pass by value. (hashcode, equals)
+		// Map<Employee, List<Employee> manList = new HashMap<>();
+		
+		Map<Integer, String> model = new HashMap<>();
+		model.put(92, "A Quang Nam");
+		model.put(43, "X Da Nang");
+		model.put(75, "B Thua Thien Hue");
+		model.put(74, "D Quang Tri");
+		
+		Map<Integer, String> sortedMap = sort(model);
+		for (Entry<Integer, String> entry : sortedMap.entrySet()) {
+			System.out.println(entry);
+		}
+	}
+
+	private static Map<Integer, String> sort(Map<Integer, String> model) {
+		List<Entry<Integer, String>> sortedList = new LinkedList<>(model.entrySet());
+		sortedList.sort(Entry.comparingByValue());
+
+		Map<Integer, String> sortedMap = new LinkedHashMap<>();
+
+		for (Entry<Integer, String> entry : sortedList) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return sortedMap;
+	}
+}
