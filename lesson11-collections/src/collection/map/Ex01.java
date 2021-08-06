b91b51c20038be699c10eb3627410025b36e690a
vhuy <vanhuy0001dtu@gmail.com>
2021-08-06 14:32:05 +0700

    lesson11-collection 06.08.2021

--- lesson11-collections/src/collection/map/Ex01.java
+++ lesson11-collections/src/collection/map/Ex01.java
@@ -2,67 +2,78 @@ package collection.map;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
-import utils.CollectionUtils;
+import java.util.function.Function;
+
+import static java.util.Comparator.*;
+
 
 public class Ex01 {
 	public static void main(String[] args) {
 		// model vehicle
-		// put >> check do hashCode(key), equals(key) exists ? existed => replace
-		// oldValue : put new
+		// put >> check do hashcode(key). equals(key) exists ? existed => replace
+		// oldValue: put new
 
-		Comparator<Integer> keyComp = new Comparator<>() {
+		// HashMap
+		// LinkedHashMap: keep right order of input elements
+		// TreeMap: Automatically sorted by Key Ascending
+
+		Comparator<Integer> keyComp = new Comparator<Integer>() {
 
 			@Override
 			public int compare(Integer k1, Integer k2) {
-				if (k1 == null) {
-					return 1;
-				}
-				if (k2 == null) {
+				if(k1 == null) {
 					return -1;
+				}
+				if(k2 == null) {
+					return 1;
 				}
 				return k2.compareTo(k1);
 			}
-
 		};
-
-		Map<Integer, String> model = new TreeMap<>();
-		model.put(43, "Da Nang");
+		Comparator<Integer> keyComp2 = nullsFirst(comparing(Function.identity()));
+		
+		Map<Integer, String> model = new TreeMap<>(keyComp2);
 		model.put(92, "Quang Nam");
-		model.putIfAbsent(43, "Da Nang 01");
-		model.put(74, "Quang Tri");
+		model.put(43, "Da Nang");
+		model.put(null, "Null");
 		model.put(75, "Thua Thien Hue");
+		model.put(74, "Quang Tri");
 
-		String v1 = model.get(88);
-		String v2 = model.getOrDefault(88, "dfvl");
-		String v3 = model.get(92);
+		String v1 = model.get(99);
+		String v2 = model.getOrDefault(99, "dfvl");
+		String v3 = model.get(75);
 
 		System.out.println("v1: " + v1);
 		System.out.println("v2: " + v2);
 		System.out.println("v3: " + v3);
 
-		String removedValue = model.remove(75);
-		System.out.println("removedValue: " + removedValue);
+		String removeValue = model.remove(75);
+		System.out.println("removedValue: " + removeValue);
 
 		System.out.println("size: " + model.size());
 
 		System.out.println("============================");
-
 		iterate(model);
-
+		System.out.println("============================");
+		iterateByKey(model);
+		System.out.println("============================");
+		iterateByValue(model);
 	}
 
+	// iterate via value
 	private static void iterateByValue(Map<Integer, String> model) {
 		Collection<String> collection = model.values();
 		for (String element : collection) {
 			System.out.println("value: " + element);
 		}
+
 	}
 
+	// iterate via key
 	private static void iterateByKey(Map<Integer, String> model) {
 		Set<Integer> keys = model.keySet();
 		for (Integer key : keys) {
@@ -70,11 +81,11 @@ public class Ex01 {
 		}
 	}
 
-	// iterate via entry
+	// iterate via key and value
 	private static void iterate(Map<Integer, String> model) {
 		Set<Entry<Integer, String>> entrySet = model.entrySet();
 		for (Entry<Integer, String> entry : entrySet) {
-			System.out.println("key: " + entry.getKey());
+			System.out.println("key:   " + entry.getKey());
 			System.out.println("value: " + entry.getValue());
 		}
 	}
