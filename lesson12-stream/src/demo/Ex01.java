008bff6e2bfbad8f054f3c8c9167a739298f60ed
Hai Dang <haidv245@gmail.com>
2021-08-10 22:26:16 +0700

    lesson12-stream 10.08.2021

---
+++ lesson12-stream/src/demo/Ex01.java
@@ -0,0 +1,103 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import bean.Apple;
+import bean.WeightType;
+import utils.AppleUtils;
+import utils.CollectionUtils;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		// input data
+		List<Apple> inventory = AppleUtils.getAll();
+		List<Apple> result = new ArrayList<Apple>();
+
+		for (Apple apple : inventory) {
+			if ("green".equals(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		System.out.println("===== Green Apples =====");
+		CollectionUtils.printf(result);
+		System.out.println("===== Red Apples =====");
+		result = filterApples(inventory, "red");
+		CollectionUtils.printf(result);
+
+		System.out.println("===== Heavy Apples =====");
+		result = filterHeavyApples(inventory, 150, WeightType.HEAVY);
+		CollectionUtils.printf(result);
+
+		System.out.println("===== Light Apples =====");
+		result = filterHeavyApples(inventory, 200, WeightType.LIGHT);
+		CollectionUtils.printf(result);
+
+		System.out.println("===== Weight and Color Apples =====");
+		result = filterApples(inventory, 280, "green");
+		CollectionUtils.printf(result);
+
+		System.out.println("===== weight and Country Apples =====");
+		result = filterApplesBy(inventory, 200, "Vietnam");
+		CollectionUtils.printf(result);
+	}
+
+	private static List<Apple> filterApples(List<Apple> inventory, String color) {
+		List<Apple> result = new ArrayList<Apple>();
+		for (Apple apple : inventory) {
+			if (color.equals(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+
+	private static List<Apple> filterHeavyApples(List<Apple> inventory, double weight, WeightType type) {
+		List<Apple> result = new ArrayList<Apple>();
+		switch (type) {
+		case HEAVY:
+			for (Apple apple : inventory) {
+				if (apple.getWeight() > weight) {
+					result.add(apple);
+				}
+			}
+			break;
+		case LIGHT:
+			for (Apple apple : inventory) {
+				if (apple.getWeight() < weight) {
+					result.add(apple);
+				}
+			}
+			break;
+		default:
+			break;
+		}
+		return result;
+	}
+
+	private static List<Apple> filterApples(List<Apple> inventory, double weight, String color) {
+		Objects.requireNonNull(weight);
+		Objects.requireNonNull(color);
+		List<Apple> result = new ArrayList<Apple>();
+		for (Apple apple : inventory) {
+			if (apple.getWeight() > weight && color.equals(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+
+	private static List<Apple> filterApplesBy(List<Apple> inventory, int weight, String country) {
+		Objects.requireNonNull(weight);
+		Objects.requireNonNull(country);
+		List<Apple> result = new ArrayList<Apple>();
+		for (Apple apple : inventory) {
+			if (apple.getWeight() > weight && country.equals(apple.getCountry())) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+
+}
---
+++ lesson12-stream/src/utils/AppleUtils.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import bean.Apple;
+
+public class AppleUtils {
+	
+	private AppleUtils() {
+		
+	}
+	
+	public static List<Apple> getAll() {
+		return Arrays.asList(
+				new Apple(1, "green", 200, "Vietnam"),	
+				new Apple(16, "green", 300, "Vietnam"),	
+				new Apple(22, "red", 300, "Lao"),	
+				new Apple(4, "yellow", 400, "Thailand"),	
+				new Apple(17, "red", 600, "Vietnam"),	
+				new Apple(6, "green", 100, "Thailand")	
+		);
+	}
+}
