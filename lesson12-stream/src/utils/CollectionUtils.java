377333abbd9b69d890648f49bd184f6924178083
ncchuong <ncchuong99@gmail.com>
2021-08-10 22:24:45 +0700

    lesson12-stream 10.08.2021

---
+++ lesson12-stream/src/utils/CollectionUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.util.List;
+
+public class CollectionUtils {
+	public CollectionUtils() {
+	
+	}
+	
+	public static <E> void printf(List<E> elements) {
+		for(E e : elements) {
+			System.out.println(e);
+		}
+	}
+}
