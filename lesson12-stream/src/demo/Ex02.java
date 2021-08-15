333e966ed5478115cf0c83ec69b44733513abca8
vhuy <vanhuy0001dtu@gmail.com>
2021-08-15 17:18:01 +0700

    lesson12-stream 15.08.2021

--- lesson12-stream/src/demo/Ex02.java
+++ lesson12-stream/src/demo/Ex02.java
@@ -1,7 +1,11 @@
 package demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
 
 import bean.Apple;
 import bean.Strategy;
@@ -11,35 +15,50 @@ import utils.CollectionUtils;
 public class Ex02 {
 	public static void main(String[] args) {
 		List<Apple> inventory = AppleUtils.getAll();
-		
-		List<Apple> greenApples = filerApples(inventory, a -> "green".equals(a.getColor()));
-		
-		List<Apple> redApples = filerApples(inventory, a -> "red".equals(a.getColor()));
-		
-		System.out.println("====green apple====");
+
+		List<Apple> greenApples = filterApples(inventory, a -> "green".equals(a.getColor()));
+
+		List<Apple> redApples = filterApples(inventory, (a) -> "red".equals(a.getColor()));
+
+		List<Apple> heavyApples = filterApples(inventory, (a) -> a.getWeight() > 200);
+
+		System.out.println("==== Green Apples ====");
 		CollectionUtils.printf(greenApples);
-		
-		System.out.println("====red apple====");
+
+		System.out.println("==== Red Apples ====");
 		CollectionUtils.printf(redApples);
+
+		System.out.println("==== Heavy Apples ====");
+		CollectionUtils.printf(heavyApples);
+		
+		System.out.println("====== Map Apples ======");
+		//lambda expressions
+		//get list of origin countries of apples
+		Set<String> colors = map(inventory, a -> a.getColor());
+		CollectionUtils.printf(colors);
+		
+		System.out.println("====== Map Length ======");
+		List<String> texts = Arrays.asList("abc", "abcde", "ab", "abcdefgh");
+		Set<Integer> lengths = map(texts, s -> s.length());
+		CollectionUtils.printf(lengths);
 		
 	}
 	
+	private static <T, R> Set<R> map(List<T> ts, Function<T, R> function){
+		Set<R> result = new HashSet<>();
+		for (T t : ts) {
+			result.add(function.apply(t));
+		}
+		return result;
+	}
 	
-/**
- * get apples with given condition
- * 
- * @param inventory input data
- * @param behavior given condition
- * @return out put data
- */
-	private static List<Apple> filerApples(List<Apple> inventory, Strategy<Apple> strategy) {
-		List<Apple> result = new ArrayList<>();
+	private static List<Apple> filterApples(List<Apple> inventory, Strategy<Apple> strategy) {
+		List<Apple> result = new ArrayList<Apple>();
 		for (Apple apple : inventory) {
-			if(strategy.behavior(apple)) {
+			if (strategy.behavior(apple)) {
 				result.add(apple);
 			}
 		}
 		return result;
 	}
-	
 }
