730b33734e662173db38ba268dc047a8de19427d
huynhquanhht <huynhquanhht@gmail.com>
2021-08-21 22:48:25 +0700

    lesson12-stream 21.08.2021

---
+++ lesson12-stream/src/demo/Ex07.java
@@ -0,0 +1,54 @@
+package demo;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import bean.Dish;
+import bean.Dish.Kind;
+import utils.DishUtils;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		// Stream is "sequences of elements from SOURCE that helps manipulate collection of data"
+		// Source: menu
+		List<Dish> menu = DishUtils.getAll();
+		
+		// menu-source -> stream -> intermediate operation -> stream -> terminal operation -> non-stream-type ()
+		Set<String> meatDishes = menu.stream()
+				 	 .filter(dish -> Kind.MEAT.equals(dish.getKind())) // Predicate
+				 	 .map(Dish::getName)
+				 	 .collect(Collectors.toSet()); 
+		meatDishes.forEach(System.out::println);
+		
+		// Not run - lazy
+		menu.stream()
+			 	 .filter(dish -> {
+			 		 System.out.println("filter: " + dish.getName());
+			 		 return Kind.MEAT.equals(dish.getKind());		 
+			 	 })
+			 	 .map(dish -> {
+			 		 System.out.println("map: " + dish.getName());
+			 		 return dish.getName();
+			 	 });
+		
+		// What is Stream ?
+		// Stream is "sequences of elements from SOURCE that helps manipulate collections of data"
+		// Stream: SOURCE - [INTERMEDIATE OPERATIONS - TERMINAL OPERATIONS] - TARGET
+		
+		// INTERMEDIATE OPERATIONS >> returned type: Stream<T>
+		// TERMINAL OPERATIONS     >> returned type: Non-Stream. E.g Array, List, Set, Map, Long, Integer
+		//                         >> count, collect(Collectors.toList(), toMap, toSet), forEach, toArray
+		
+		// Steps: menu-source -> stream -> intermediate operation -> stream -> terminal operation -> non-stream type (long, collection, array)
+		// Features:
+		// 1. Traversable once only
+		// 2. Stream & Collections
+		// Same: Can store and manipulate with data
+		// Diff: Collection: Focus on store data
+		//       Stream    : Focus on manipulate data, 
+		// 3. Intermediate operations will do nothing until a terminal operation invoked
+		// 4. Internal iteration
+		// 5. Express a query than ad hoc implementation	
+	}
+}
