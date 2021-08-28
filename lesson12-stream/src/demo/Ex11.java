f4f8990b7f2ccc2e33c08c3c5d815364df5b8b0c
ncchuong <ncchuong99@gmail.com>
2021-08-28 22:45:11 +0700

    edit Ex11.java Lesson12-stream

--- lesson12-stream/src/demo/Ex11.java
+++ lesson12-stream/src/demo/Ex11.java
@@ -5,34 +5,30 @@ import java.util.OptionalInt;
 
 public class Ex11 {
 	public static void main(String[] args) {
-
+		// 1, 2, 1, 4, 5, 8, 10, 4, 12
 		int[] digits = { 1, 2, 1, 4, 5, 8, 10, 4, 12 };
 
-		// 1
-		boolean d10 = Arrays.stream(digits).anyMatch(d -> d % 10 == 0);
+		// 1st: is there any element that is divisible by 10
+		boolean d10 = Arrays.stream(digits).anyMatch(item -> item % 10 == 0);
 		System.out.println("anyD10: " + d10);
 
-		// 2
-		OptionalInt optIntD4 = Arrays.stream(digits)
-					.filter(i -> i % 4 == 0).findFirst();
-		if(optIntD4.isPresent()) {
-			System.out.println("firstD4" + optIntD4.getAsInt());
+		// 2nd: find the first element that is divisible by 4
+		OptionalInt opIntD4 = Arrays.stream(digits).filter(item -> item % 4 == 0).findFirst();
+		if (opIntD4.isPresent()) {
+			System.out.println("firstD4: " + opIntD4.getAsInt());
 		}
-		
-		//3 findAny = findFirst
-		OptionalInt optIntD5 = Arrays.stream(digits)
-				.filter(i -> i % 5 == 0).findAny();
-		if(optIntD4.isPresent()) {
-			System.out.println("firstD4" + optIntD5.getAsInt());
+
+		// 3nd: find the element that is divisible by 5 in the list
+		OptionalInt opIntD5 = Arrays.stream(digits).filter(item -> item % 5 == 0).findAny();
+		if (opIntD5.isPresent()) {
+			System.out.println("anyD5: " + opIntD5.getAsInt());
 		}
-		
-		//4:
-		boolean allL100 = Arrays.stream(digits).allMatch(d -> d > 100);
-		System.out.println(allL100);
-		
-		//5:
-		boolean noneGt0  = Arrays.stream(digits).noneMatch(d -> d > 0);
-		System.out.println("noneGT0: " + noneGt0);
-		
+		// 4th: does all elements less than 100 // allMath
+		boolean allLT100 = Arrays.stream(digits).allMatch(item -> item < 100);
+		System.out.println("allLt100: " + allLT100);
+
+		// 5th: does no elements greater than 0 // noneMath
+		boolean noneGT100 = Arrays.stream(digits).noneMatch(item -> item > 0);
+		System.out.println("noneGt100: " + noneGT100);
 	}
 }
