9c4a38c4382b3149f05dc7e299344550e9d9906c
Hai Dang <haidv245@gmail.com>
2021-08-28 22:40:59 +0700

    lesson12-stream 28.08.2021

--- lesson12-stream/src/bean/Dish.java
+++ lesson12-stream/src/bean/Dish.java
@@ -1,71 +1,86 @@
 package bean;
 
 public class Dish {
-	
+
 	public enum Kind {
 		MEAT, FISH, OTHER
 	}
-	
-    private String id;
-    private String name;
-    private double calories;
-    private Kind kind;
-    private boolean vegetarian;
 
+	private String id;
+	private String name;
+	private double calories;
+	private Kind kind;
+	private boolean vegetarian;
 
-    public Dish() {
-    }
+	public Dish() {
+	}
 
-    public Dish(String id, String name, double calories, Kind kind, boolean vegetarian) {
-        this.id = id;
-        this.name = name;
-        this.calories = calories;
-        this.kind = kind;
-        this.vegetarian = vegetarian;
-    }
+	public Dish(String id, String name, double calories, Kind kind, boolean vegetarian) {
+		this.id = id;
+		this.name = name;
+		this.calories = calories;
+		this.kind = kind;
+		this.vegetarian = vegetarian;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public double getCalories() {
-        return calories;
-    }
+	public double getCalories() {
+		return calories;
+	}
 
-    public void setCalories(double calories) {
-        this.calories = calories;
-    }
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
 
-    public Kind getKind() {
+	public Kind getKind() {
 		return kind;
 	}
-    
-    public void setKind(Kind kind) {
+
+	public void setKind(Kind kind) {
 		this.kind = kind;
 	}
-    
-    public boolean isVegetarian() {
-        return vegetarian;
-    }
 
-    public void setVegetarian(boolean vegetarian) {
-        this.vegetarian = vegetarian;
-    }
-    
-    @Override
-    public String toString() {
-        return id + ", " + name + ", " + calories + ", " + kind + ", " + vegetarian;
-    }
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	public void setVegetarian(boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
+
+	@Override
+	public String toString() {
+		return id + ", " + name + ", " + calories + ", " + kind + ", " + vegetarian;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (this == o) {
+			return true;
+		}
+
+		if (!(o instanceof Dish)) {
+			return false;
+		}
+
+		Dish that = (Dish) o;
+
+		return this.getCalories() == that.getCalories();
+	}
 }
--- lesson12-stream/src/demo/Ex08.java
+++ lesson12-stream/src/demo/Ex08.java
@@ -2,7 +2,11 @@ package demo;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import utils.ArrayUtils;
@@ -11,7 +15,7 @@ import utils.CollectionUtils;
 public class Ex08 {
 	public static void main(String[] args) {
 
-		int[] digits = { 1, 2, 1, 3, 3, 2, 5 };
+		int[] digits = { 1, 2, 1, 3, 3, 2, 5, 7 };
 		int[] oddNumbers = Arrays.stream(digits).filter(e -> e % 2 != 0).toArray();
 		ArrayUtils.printf(oddNumbers);
 		// exercise select unique elements from input list of numbers
@@ -21,5 +25,14 @@ public class Ex08 {
 		Set<Integer> eventNumbers = numbers.stream().filter(e -> e % 2 == 0).collect(Collectors.toSet());
 		CollectionUtils.printf(eventNumbers);
 		System.out.println("runtime class: " + eventNumbers.getClass());
+
+		// Ex01
+		List<Integer> nums = Arrays.stream(digits).boxed().collect(Collectors.toList());
+		List<Integer> result = nums.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.entrySet().stream()
+				.filter(e -> e.getValue() == 1)
+				.map(Entry::getKey).collect(Collectors.toList());
+		System.out.println(result);
 	}
 }
