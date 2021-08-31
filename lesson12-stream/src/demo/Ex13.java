1c7659801052f12e45a445a49a3ba7cc051682d9
huongmy <huongmydn@gmail.com>
2021-08-31 22:09:32 +0700

    lesson12-stream 31.08.2021

---
+++ lesson12-stream/src/demo/Ex13.java
@@ -0,0 +1,32 @@
+package demo;
+
+import java.util.List;
+import java.util.Optional;
+
+import bean.Dish;
+import utils.DishUtils;
+
+/**
+ * How could you calculate sum of the calories in the menu
+ */
+public class Ex13 {
+	public static void main(String[] args) {
+		List<Dish> menu = DishUtils.getAll();
+		
+		double sumOfCalories = menu.stream()
+				.map(Dish::getCalories) // Stream<Double>
+				.reduce(0d, Double::sum);
+		System.out.println("sumOfCalories: " + sumOfCalories);
+		
+		// Primitive Stream: IntStream, LongStream, DoubleStream
+		double priSumOfCalories = menu.stream() // Stream<Dish>
+				.mapToDouble(Dish::getCalories) // DoubleStream
+				.sum();
+		System.out.println("priSumOfCalories: " + priSumOfCalories);
+		
+		// How could you find dish with maximum calories
+		Optional<Dish> dishOpt = menu.stream().reduce(Dish::maxClr);
+		dishOpt.ifPresent(System.out::println);
+	}
+	
+}
