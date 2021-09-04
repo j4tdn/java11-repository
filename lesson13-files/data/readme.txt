6b57ce8e3b1902c1a4fda344c071a0851ef6e0d5
huongmy <huongmydn@gmail.com>
2021-09-04 22:27:57 +0700

    lesson13-files

---
+++ lesson13-files/data/readme.txt
@@ -0,0 +1,8 @@
+Line1
+Line2
+Line1
+Line2
+Line3
+Line4
+Transaction: Alan - Milan 25000
+Trader: Kyler >> Cambridge 100000
---
+++ lesson13-files/src/common/Extension.java
@@ -0,0 +1,19 @@
+package common;
+
+public enum Extension {
+	mp4(".mp4"),
+	mp3(".mp3"),
+	txt(".txt"),
+	java(".java"),
+	png(".png");
+	
+	private final String ext;
+	
+	Extension(String ext) {
+		this.ext = ext;
+	}
+	
+	public String val() {
+		return ext;
+	}
+}
