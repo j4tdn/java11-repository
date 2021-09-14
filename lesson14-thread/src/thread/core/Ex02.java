24dfa06e60740f0bc5118bcdb80c8d2b356ce62e
hpham <binhmt2008@gmail.com>
2021-09-14 22:41:44 +0700

    lesson14-thread 14.09.2021

---
+++ lesson14-thread/src/thread/core/Ex02.java
@@ -0,0 +1,36 @@
+package thread.core;
+
+import static utils.ThreadUtils.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class Ex02 {
+
+	// thread main
+	public static void main(String[] args) {
+
+		System.out.println("main start");
+
+		Thread t1 = new Thread(new Task(), "thread1");
+		t1.start();
+
+		// curent thread
+		printCurrentThreadName();
+		System.out.println("main end");
+	}
+
+	private static class Task implements Runnable {
+
+		@Override
+		public void run() {
+			// prepare data took 2s
+			doTask(2, TimeUnit.SECONDS);
+
+			printCurrentThreadName();
+			System.out.println("running a task ....");
+
+		}
+
+	}
+
+}
---
+++ lesson14-thread/src/thread/core/Ex04.java
@@ -0,0 +1,54 @@
+package thread.core;
+
+import static utils.ThreadUtils.*;
+import static utils.ThreadUtils.startThread;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.*;
+
+public class Ex04 {
+	// Task: Took 3 seconds
+	// Demo performance case with 3 calculation tasks
+	// Run single task : Took 9 seconds
+	// Run parallel with 3 threads : Took 3 seconds
+
+	private static long start = 0;
+
+	public static void main(String[] args) {
+		Task task1 = new Task(3, SECONDS);
+		Task task2 = new Task(4, SECONDS);
+		Task task3 = new Task(5, SECONDS);
+
+		start = System.currentTimeMillis();
+
+		Thread t1 = new Thread(task1, "thread1");
+		t1.start();
+
+		Thread t2 = new Thread(task2, "thread2");
+		t2.start();
+
+		Thread t3 = new Thread(task3, "thread3");
+		t3.start();
+
+	}
+
+	private static class Task implements Runnable {
+		private long time;
+		private TimeUnit unit;
+
+		public Task(long time, TimeUnit unit) {
+			this.time = time;
+			this.unit = unit;
+		}
+
+		@Override
+		public void run() {
+			startThread();
+			doTask(3, TimeUnit.SECONDS);
+			System.out.println(getThreadName() + "Tooks " + (System.currentTimeMillis() - start) + "ms");
+
+		}
+
+	}
+}
