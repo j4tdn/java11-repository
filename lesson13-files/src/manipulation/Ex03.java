b5cc5539c262ddbe13270fda0138a2f685d6a8df
huynhquanhht <huynhquanhht@gmail.com>
2021-09-17 03:32:33 +0700

    lesson13-files 11.09.2021

--- lesson13-files/src/manipulation/Ex03.java
+++ lesson13-files/src/manipulation/Ex03.java
@@ -1,5 +1,10 @@
 package manipulation;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import common.Trader;
 import utils.DataUtils;
@@ -7,13 +12,15 @@ import utils.FileUtils;
 
 public class Ex03 {
 	private static final String TRADER_PATH = "trader.txt";
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		File file = new File(TRADER_PATH);
-		 List<Trader> traders = DataUtils.getTraders();
-		 FileUtils.writeObjects(file, traders);
-		 FileUtils.open(file);
-		 System.out.println("=======");
-		
+		List<Trader> traders = DataUtils.getTraders();
+		FileUtils.writeObjects(file, traders);
+		FileUtils.open(file);
+		System.out.println("=======");
+		Path path = Paths.get("data/ex03-input.txt");
+		List<String> lines = Files.readAllLines(path);
 		List<Trader> traderss = FileUtils.readObject(file);
 		System.out.println(traderss);
 	}
