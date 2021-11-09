d530152afbc833c7ecc166a547f9c827a81b2af0
vhuy <vanhuy0001dtu@gmail.com>
2021-11-09 23:08:27 +0700

    lesson16-index-procedure-function 09.11.2021

--- sql dml.sql
+++ sql dml.sql
@@ -24,3 +24,70 @@ WHERE MaMH = 4;
 DELETE FROM LoaiHang
 WHERE MaLH = 3;	
 ROLLBACK;
+
+SELECT * 
+FROM mathang;
+
+-- concatenate (... Strings)
+SELECT MaMH,
+	   TenMH,
+       MauSac,
+       concat(TenMH, '-' , MauSac) AS ChiTiet,
+       current_timestamp() ThoiGian
+FROM mathang;
+
+-- group concate(column)
+SELECT GROUP_CONCAT(MaMH SEPARATOR '-')
+FROM mathang;
+
+SELECT DISTINCT MauSac
+FROM mathang
+LIMIT 5;
+
+SELECT *, IF(GiaBan - GiaMua >= 30, GiaBan-15, GiaBan) GiaGiam
+FROM chitietmathang
+ORDER BY MaKC DESC, GiaBan ASC;
+
+-- group by
+SELECT * FROM mathang;
+SELECT COUNT(MaMH) FROM mathang;
+
+SELECT MaLH, COUNT(*) SoLuongMatHang
+FROM mathang
+WHERE MaLH IN (1, 3, 5)
+GROUP BY MaLH
+HAVING SoLuongMatHang > 2;
+
+UPDATE mathang
+SET MauSac = NULL
+WHERE MaMH IN (2,5,7);
+
+SELECT MaMH 
+FROM mathang
+WHERE MaLH = 2
+UNION ALL
+SELECT MaMH
+FROM chitietdonhang;
+
+-- phép kết - join
+SELECT *FROM mathang;
+SELECT *FROM loaihang;
+INSERT INTO loaihang(MaLH, TenLH)
+VALUES(6, 'Tất');
+-- liệt kê tất cả các mặt hàng thuộc loại hàng là áo
+SELECT * FROM mathang
+WHERE MaLH = (SELECT MaLH FROM loaihang WHERE TenLH LIKE '%Áo%');
+
+-- join
+SELECT * 
+FROM mathang
+JOIN loaihang
+ON mathang.MaLH = loaihang.MaLH
+WHERE loaihang.TenLH LIKE '%Áo%';
+
+-- right join
+SELECT * 
+FROM mathang
+RIGHT JOIN loaihang
+ON mathang.MaLH = loaihang.MaLH;
+
