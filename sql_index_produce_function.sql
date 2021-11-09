2470c2bccab70458cfd93675765aedc009152832
HungHungGNX <79443809+HungHungGNX@users.noreply.github.com>
2021-11-09 23:07:40 +0700

    index produce function 9.11.21

---
+++ sql_index_produce_function.sql
@@ -0,0 +1,110 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@HungHungGNX 
+j4tdn
+/
+java11-repository
+Public
+2
+10
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+java11-repository/DATABASE/sql_index_produce_function.sql
+@giabao2807
+giabao2807 index produce function 9.11.21
+Latest commit 3a43a92 1 minute ago
+ History
+ 1 contributor
+66 lines (55 sloc)  1.29 KB
+   
+-- INDEX
+SELECT * FROM mathang WHERE MauSac = 'Xanh';
+EXPLAIN SELECT * FROM mathang WHERE MauSac = 'Xanh';
+
+CREATE INDEX IDX_COLOR
+ON mathang(MauSac);
+
+-- PROCEDURE - FUNCION
+-- WHEN: cannot use availiable DML. DDL to query
+
+DELIMITER $$
+CREATE PROCEDURE selectItems()
+BEGIN
+-- code
+SELECT * FROM mathang;
+END $$
+DELIMITER ;
+CALL selectItems();
+
+-- liet ke cac mat hang theo loai hang
+DROP PROCEDURE IF EXISTS selectItemsByGroupId;
+DELIMITER $$
+CREATE PROCEDURE selectItemsByGroupId(p_id int)
+BEGIN
+-- code
+SELECT * FROM mathang WHERE MaLH = p_id;
+END $$
+DELIMITER ;
+CALL selectItemsByGroupId(2);
+
+-- them n dong du lieu cho bang loai hang
+DELIMITER $$
+CREATE PROCEDURE addItemGroups(n int)
+BEGIN
+	DECLARE i INT DEFAULT 1;
+	DECLARE current_igid INT DEFAULT (SELECT MAX(MaLH) FROM loaihang);
+    
+	WHILE i <= n DO
+		INSERT INTO LoaiHang(MaLH, TenLH)
+        VALUES (current_igid + i, concat('LH' , current_igid + i));
+        SET i = i + 1;
+	END WHILE;
+END $$
+DELIMITER ;
+
+CALL addItemGroups(5);
+SELECT * from loaihang;
+
+SET GlOBAL log_bin_trust_function_creators = 1;
+DELIMITER $$
+CREATE FUNCTION sumOf(n int)
+RETURNS INT
+BEGIN
+	DECLARE i INT DEFAULT 1;
+	DECLARE sumOf INT DEFAULT 0;
+    
+	WHILE i <= n DO
+		SET sumOf = sumOf + i;
+        SET i = i + 1;
+	END WHILE;
+    
+    RETURN sumOf;
+END $$
+DELIMITER ;
+
+SELECT sumOf(10);
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
+1
