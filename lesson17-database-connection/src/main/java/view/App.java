eea627849636601a49068231698df99057c6fb07
teoven <68175492+teoven@users.noreply.github.com>
2021-11-16 23:12:41 +0700

    lesson17-database-connection 16.11.2021

---
+++ lesson17-database-connection/src/main/java/view/App.java
@@ -0,0 +1,34 @@
+package view;
+
+import java.util.List;
+
+import persistence.ItemGroup;
+import service.ItemGroupService;
+import service.ItemGroupServiceImpl;
+import  static utils.SqlUtils.*;
+
+public class App {
+	
+	private static int igId;
+	private static ItemGroup newGroup;
+	private static ItemGroupService itemGroupService;
+	
+	static {
+		itemGroupService = new ItemGroupServiceImpl();
+		
+		igId = 3;
+		newGroup = new ItemGroup(999, "Saved Item group 999");
+	}
+	
+	public static void main(String[] args) {
+		List<ItemGroup> groups = itemGroupService.getAll();
+		print(groups);
+		
+		System.out.println("\n==\\\\\\== \n ");
+		ItemGroup group = itemGroupService.get(igId);
+		print("group", group);
+		
+		boolean isSavedorUpdate = itemGroupService.saveOrUpdate(newGroup);
+		print("isSaved", isSavedorUpdate);
+	}
+}
