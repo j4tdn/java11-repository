b8d80c7a2e4f31f608ca849d09ccf9a4d1f8f02c
Ngo Viet Cuong <ngocuong12092001@gmail.com>
2021-11-23 19:57:45 +0700

    database connection 23.11.2021

---
+++ lesson17-database-connection/src/main/java/view/App.java
@@ -0,0 +1,36 @@
+package view;
+
+import java.util.List;
+
+import persistence.ItemGroup;
+import service.ItemGroupService;
+import service.ItemGroupServiceImpl;
+import static utils.SqlUtils.*;
+
+public class App {
+	
+	private static int igId;
+	private static ItemGroup newGroup;
+	private static ItemGroupService itemGroupService;
+	
+	static {
+		itemGroupService = new ItemGroupServiceImpl();
+		igId = 5;
+		newGroup = new ItemGroup(999, "Save Item Group 123");
+	}
+	
+	public static void main(String[] args) {
+		List<ItemGroup> groups = itemGroupService.getAll();
+		print(groups);
+		
+		System.out.println("\n ==\\\\\\\\\\\\== \n");
+		
+		ItemGroup group = itemGroupService.get(igId);
+	    print("group", group);
+	    
+	    System.out.println("\n ==\\\\\\\\\\\\== \n");
+	    
+	    boolean saveOrUpdate = itemGroupService.saveOrUpdate(newGroup);
+	    print("saveOrUpdate", saveOrUpdate);
+	}
+}
