625addd78c77ff3132108166bf2e48ec4b419a25
hoadzzz <levanhoa190698@gmail.com>
2021-12-14 22:26:18 +0700

    lesson18-hibernate continue 2 14.12.2021

--- lesson18-hibernate/src/main/java/demo/App.java
+++ lesson18-hibernate/src/main/java/demo/App.java
@@ -1,12 +1,26 @@
 package demo;
 
-import org.hibernate.SessionFactory;
+import java.util.List;
 
-import provider.HibernateProvider;
+import persistence.Item;
+import persistence.ItemGroup;
+import service.ItemGroupService;
+import service.ItemGroupServiceImpl;
+import service.ItemService;
+import service.ItemServiceImpl;
+import utils.SqlUtils;
 
 public class App {
 	public static void main(String[] args) {
-		SessionFactory sessionFactory = HibernateProvider.getSessionFactory();
-		System.out.println(sessionFactory);
+		ItemGroupService itemGroupService = new ItemGroupServiceImpl();
+		List<ItemGroup> itemGroups = itemGroupService.getAll();
+		System.out.println("============================");
+		SqlUtils.print(itemGroups);
+		
+		System.out.println("=============================");
+		ItemService itemService = new ItemServiceImpl();
+		List<Item> items = itemService.getAll();
+		SqlUtils.print(items);
+		
 	}
 }
