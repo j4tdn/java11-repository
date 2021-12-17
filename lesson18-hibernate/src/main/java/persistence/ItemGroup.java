8f5907894d1c1528b52504cf4b6ddb200d6dce7d
ncchuong <ncchuong99@gmail.com>
2021-12-17 18:08:34 +0700

    lesson18-hibernate 17.12.2021

--- lesson18-hibernate/src/main/java/persistence/ItemGroup.java
+++ lesson18-hibernate/src/main/java/persistence/ItemGroup.java
@@ -1,27 +1,42 @@
 package persistence;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 // Mapped with LoaiHang table
-// Entity: Java class used for mapping properties with table columns
-// >> Get from Table >> Table's properties automatically set into corresponding Class's properties
+// Entity: Java Class used for mapping properties with table columns
+// >> Get from Table >> Table's columns automatically set value into corresponding Class's properties 
 
 @Entity
 @Table(name = "LoaiHang")
+@NamedQueries({
+	@NamedQuery(name = ItemGroup.Q_GET_ALL, query = "FROM ItemGroup")
+})
 public class ItemGroup {
 	
+	public static final String Q_GET_ALL = "Q_GET_ALL";
+
 	@Id
 	@Column(name = "MaLH")
 	private Integer id;
-	
+
 	@Column(name = "TenLH")
 	private String name;
 	
-	// Mapping 1-N with Item
+	@OneToMany(mappedBy = "itemGroup")
+	private List<Item> items;
 	
+	// fetch type
+	// @OneToMany: default LAZY
+
 	/**
 	 * Hibernate Empty Constructor
 	 */
@@ -32,7 +47,7 @@ public class ItemGroup {
 		this.id = id;
 		this.name = name;
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
@@ -48,11 +63,17 @@ public class ItemGroup {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public List<Item> getItems() {
+		return items;
+	}
+	
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
 
 	@Override
 	public String toString() {
 		return "ItemGroup [id=" + id + ", name=" + name + "]";
 	}
-	
-	
 }
--- lesson18-hibernate/src/main/java/provider/HibernateProvider.java
+++ lesson18-hibernate/src/main/java/provider/HibernateProvider.java
@@ -6,54 +6,58 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
+import persistence.Item;
 import persistence.ItemGroup;
 
 public class HibernateProvider {
 	
 	private static SessionFactory sessionFactory;
-	private static String HIBARNATE_PROERTIES_PATH = "hibernate.cfg.xml";
+	// private static String HIBERNATE_PROPERTIES_PATH = "hibernate.cfg.xml"; 
 	
 	private HibernateProvider() {
-		
 	}
 	
+	/**
+	 * XML Configuration
+	 */
 	/*
 	public static SessionFactory getSessionFactory() {
-		if(sessionFactory == null) {
+		if (sessionFactory == null) {
 			Configuration configuration = new Configuration();
-			sessionFactory = configuration.configure(HIBARNATE_PROERTIES_PATH).buildSessionFactory();
+			sessionFactory = configuration.configure(HIBERNATE_PROPERTIES_PATH).buildSessionFactory();
 		}
 		
 		return sessionFactory;
-	}
-	*/
+	}*/
 	
+	/**
+	 * JAVA Configuration
+	 */
 	public static SessionFactory getSessionFactory() {
-		if(sessionFactory == null) {
+		if (sessionFactory == null) {
 			Configuration configuration = new Configuration();
 			
-			// Scan Entities
 			configuration.addAnnotatedClass(ItemGroup.class);
+			configuration.addAnnotatedClass(Item.class);
 			
-			sessionFactory = configuration.setProperties(getHivernateProps()).buildSessionFactory();
+			sessionFactory = configuration.setProperties(getHibernateProps())
+										  .buildSessionFactory();
 		}
-		
 		return sessionFactory;
 	}
 	
-	private static Properties getHivernateProps() {
+	private static Properties getHibernateProps() {
 		Properties props = new Properties();
-
+		
 		props.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
 		props.put(Environment.URL, "jdbc:mysql://localhost:3306/java11_shopping");
 		props.put(Environment.USER, "root");
 		props.put(Environment.PASS, "1234");
+		
 		props.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 		props.put(Environment.SHOW_SQL, "true");
 		props.put(Environment.FORMAT_SQL, "true");
 		props.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-		
-		
 		
 		return props;
 	}
