0b0796a0ac005a8a00d08feeea88dbf504211f73
HoangSon <hthaison17@gmail.com>
2022-01-03 13:30:47 +0700

    lesson18-hthaison-hibernate-late

---
+++ lesson18-hibernate/src/main/java/persistence/ItemGroup.java
@@ -0,0 +1,86 @@
+package persistence;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+// Mapped with LoaiHang table
+// Entity: Java Class used for mapping properties with table columns
+// >> Get from Table >> Table's columns automatically set value into corresponding Class's properties 
+
+@Entity
+@Table(name = "LoaiHang")
+@NamedQueries({
+	@NamedQuery(name = ItemGroup.Q_GET_ALL, query = "FROM ItemGroup")
+})
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class ItemGroup {
+	
+	public static final String Q_GET_ALL = "Q_GET_ALL";
+
+	@Id
+	@Column(name = "MaLH")
+	private Integer id;
+
+	@Column(name = "TenLH")
+	private String name;
+	
+	@OneToMany(mappedBy = "itemGroup")
+	private List<Item> items;
+	
+	// fetch type
+	// @OneToMany: default LAZY
+
+	/**
+	 * Hibernate Empty Constructor
+	 */
+	public ItemGroup() {
+	}
+
+	public ItemGroup(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Item> getItems() {
+		return items;
+	}
+	
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		// N+1 problem >> 
+		
+		// EAGER
+		// A > B > C > D
+		return "ItemGroup [id=" + id + ", name=" + name + "]";
+	}
+}
---
+++ lesson18-hibernate/src/main/java/service/ItemGroupService.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.util.List;
+
+import persistence.ItemGroup;
+import persistence.ItemGroupDto;
+
+public interface ItemGroupService {
+	List<ItemGroup> getAll();
+	
+	List<ItemGroupDto> getItemsByItemGroupId();
+
+	void demoSlvCache();
+}
