5c4504cd7e2afb4f8180f81864a1501f7a5805ed
Thai Luc <thaitangluc2412@gmail.com>
2022-04-26 21:42:06 +0700

    bootstrap 26.04.2-2022

---
+++ Bootstrap5/js/custom.js
@@ -0,0 +1,22 @@
+
+window.onresize = function() {
+    let windowWidth = window.innerWidth;
+    document.querySelector('#width').textContent = windowWidth;
+
+    let brpVal = '';
+    if (windowWidth < 576) {
+        brpVal = 'xs';
+    } else if (windowWidth < 767){
+        brpVal = 'sm';
+    } else if (windowWidth < 992) {
+        brpVal = 'md';
+    } else if (windowWidth < 1200) {
+        brpVal = 'lg';
+    } else if (windowWidth < 1400){
+        brpVal = 'xl';
+    } else {
+        brpVal = 'xxl';
+    }
+    document.querySelector('#breakpoint').textContent = brpVal;
+}
+  
